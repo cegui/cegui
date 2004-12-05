4e7cd7e3b57ef7bdd70a9edb21e00da89cc385dc
James O'Sullivan <josullivan@users.sourceforge.net>
2004-12-05 18:27:18 +0000

    Initial commit of the resource provider.

---
+++ include/CEGUIResourceProvider.h
@@ -0,0 +1,108 @@
+/************************************************************************
+	filename: 	CEGUIResourceProvider.h
+	created:	8/7/2004
+	author:		James '_mental_' O'Sullivan
+	
+	purpose:	Defines abstract base class for loading DataContainer objects
+*************************************************************************/
+/*************************************************************************
+    Crazy Eddie's GUI System (http://crayzedsgui.sourceforge.net)
+    Copyright (C)2004 Paul D Turner (crayzed@users.sourceforge.net)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*************************************************************************/
+#ifndef _CEGUIResourceProvider_h_
+#define _CEGUIResourceProvider_h_
+
+#include "CEGUIBase.h"
+#include "CEGUISingleton.h"
+#include "CEGUIDataContainer.h"
+
+
+// Start of CEGUI namespace section
+namespace CEGUI
+{
+/*!
+\brief
+	Abstract class that defines the required interface for all resource provider sub-classes.
+
+	A ResourceProvider is used to load both XML and binary data from an external source.  This could be from a filesystem or the resource manager of a specific renderer.
+*/
+class CEGUIBASE_API ResourceProvider : public Singleton<ResourceProvider>
+{
+public:
+	/*************************************************************************
+		Construction and Destruction
+	*************************************************************************/
+    /*!
+    \brief
+        Constructor for the ResourceProvider class
+    */
+	ResourceProvider() { }
+
+    /*!
+    \brief
+        Destructor for the ResourceProvider class
+    */
+	virtual ~ResourceProvider(void) { }
+
+    /*************************************************************************
+        Accessor functions
+    *************************************************************************/
+    /*!
+    \brief
+        Return singleton ResourceProvider object
+
+    \return
+        Singleton WindowManager object
+    */
+
+    static ResourceProvider& getSingleton(void);
+
+    /*!
+    \brief
+        Return pointer to singleton WindowManager object
+
+    \return
+        Pointer to singleton WindowManager object
+    */
+    static ResourceProvider* getSingletonPtr(void);
+
+    /*!
+    \brief
+        Load XML data using InputSource objects.
+
+    \param
+        String containing a filename of the resource to be loaded.
+        Reference to a InputSourceContainer object to load the data into.
+ialized.
+    */
+    virtual void loadInputSourceContainer(const String& filename, InputSourceContainer& output) = 0;
+
+    /*!
+    \brief
+        Load raw binary data.
+
+    \param
+        String containing a filename of the resource to be loaded.
+        Reference to a RawDataContainer object to load the data into.
+ialized.
+    */
+    virtual void loadRawDataContainer(const String& filename, RawDataContainer&) = 0;
+};
+
+} // End of  CEGUI namespace section
+
+#endif	// end of guard _CEGUIResourceProvider_h_
---
+++ src/renderers/OgreGUIRenderer/OgreResourceProvider.cpp
@@ -0,0 +1,65 @@
+/************************************************************************
+	filename: 	OgreResourceProvider.cpp
+	created:	8/7/2004
+	author:		James '_mental_' O'Sullivan
+	
+	purpose:	Implements the Resource Provider common functionality
+*************************************************************************/
+/*************************************************************************
+    Crazy Eddie's GUI System (http://crayzedsgui.sourceforge.net)
+    Copyright (C)2004 Paul D Turner (crayzed@users.sourceforge.net)
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*************************************************************************/
+#include "renderers/OgreGUIRenderer/OgreResourceProvider.h"
+
+#include "CEGUIExceptions.h"
+#include "OgreArchiveManager.h"
+
+#include <xercesc/framework/MemBufInputSource.hpp>
+
+// Start of CEGUI namespace section
+namespace CEGUI
+{
+    void OgreResourceProvider::loadInputSourceContainer(const String& filename, InputSourceContainer& output)
+    {
+        Ogre::DataChunk input;
+        if( !Ogre::ArchiveManager::getSingleton()._findResourceData(
+            filename.c_str(), input ) )
+        {
+            throw InvalidRequestException((utf8*)
+                "Scheme::Scheme - Filename supplied for Scheme loading must be valid");
+        }
+
+        XERCES_CPP_NAMESPACE_USE
+        InputSource* mInputSource = new MemBufInputSource(input.getPtr(), input.getSize(), filename.c_str(), false);
+
+        output.setData(mInputSource);
+    }
+
+    void OgreResourceProvider::loadRawDataContainer(const String& filename, RawDataContainer& output)
+    {
+        Ogre::DataChunk input;
+        if( !Ogre::ArchiveManager::getSingleton()._findResourceData(
+            filename.c_str(), input ) )
+        {
+            throw InvalidRequestException((utf8*)
+                "Scheme::Scheme - Filename supplied for Scheme loading must be valid");
+        }
+
+        output.setData(input.getPtr());
+        output.setSize(input.getSize());
+    }
+} // End of  CEGUI namespace section
