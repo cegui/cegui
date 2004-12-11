76fdf1be5571c618f0a9fcf1fb130fe4ca3a2868
Paul Turner <paul@cegui.org.uk>
2004-12-11 12:30:41 +0000

    Fix for some warnings on MSVC++ 6.

--- WidgetSets/TaharezLook/include/TLButton.h
+++ WidgetSets/TaharezLook/include/TLButton.h
@@ -34,6 +34,12 @@
 #include "TLButtonProperties.h"
 
 
+#if defined(_MSC_VER)
+#	pragma warning(push)
+#	pragma warning(disable : 4251)
+#endif
+
+
 // Start of CEGUI namespace section
 namespace CEGUI
 {
@@ -342,5 +348,9 @@ public:
 
 } // End of  CEGUI namespace section
 
+
+#if defined(_MSC_VER)
+#	pragma warning(pop)
+#endif
 
 #endif	// end of guard _TLButton_h_
