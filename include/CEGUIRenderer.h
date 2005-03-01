23ef807778b162ed37b4ee9dcfab1645db389377
Paul Turner <paul@cegui.org.uk>
2005-03-01 10:25:11 +0000

    Removed incorrect usage of Win32 dll import/export construct from enums.

--- include/CEGUIRenderer.h
+++ include/CEGUIRenderer.h
@@ -50,7 +50,7 @@ enum OrientationFlags {
 \brief
 	Enumerated type that contains the valid diagonal-mode that specify how a quad is split into triangles when rendered with fx. a 3D API
 */
-enum CEGUIBASE_API QuadSplitMode
+enum QuadSplitMode
 {
 	TopLeftToBottomRight,	//!< Diagonal goes from top-left to bottom-right
 	BottomLeftToTopRight	//!< Diagonal goes from bottom-left to top-right
