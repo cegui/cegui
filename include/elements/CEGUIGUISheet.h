3241dd0f3fb6efc63ad7c6fe593a30698205f4a1
Paul Turner <paul@cegui.org.uk>
2005-03-10 14:03:31 +0000

    Added typedef to define DefaultWindow.

--- include/elements/CEGUIGUISheet.h
+++ include/elements/CEGUIGUISheet.h
@@ -1,9 +1,9 @@
 /************************************************************************
-	filename: 	CEGUIGUISheet.h
-	created:	5/6/2004
-	author:		Paul D Turner
-	
-	purpose:	Interface to a default window
+    filename:   CEGUIGUISheet.h
+    created:    5/6/2004
+    author:     Paul D Turner
+
+    purpose:    Interface to a default window
 *************************************************************************/
 /*************************************************************************
     Crazy Eddie's GUI System (http://www.cegui.org.uk)
@@ -35,107 +35,113 @@ namespace CEGUI
 {
 /*!
 \brief
-	Window class intended to be used as a simple, generic Window.
+    Window class intended to be used as a simple, generic Window.
 
-	This class does no rendering and so appears totally transparent.  This window defaults
-	to position 0.0f, 0.0f with a size of 1.0f x 1.0f.
+    This class does no rendering and so appears totally transparent.  This window defaults
+    to position 0.0f, 0.0f with a size of 1.0f x 1.0f.
 
-	/note
-	The C++ name of this class has been retained for backward compatibility reasons.  The intended usage of
-	this window type has now been extended beyond that of a gui-sheet or root window.
+    /note
+    The C++ name of this class has been retained for backward compatibility reasons.  The intended usage of
+    this window type has now been extended beyond that of a gui-sheet or root window.
 */
 class CEGUIBASE_API GUISheet : public Window
 {
 public:
-	/*************************************************************************
-		Constants
-	*************************************************************************/
-	// type name for this widget
-	static const String WidgetTypeName;				//!< The unique typename of this widget
+    /*************************************************************************
+        Constants
+    *************************************************************************/
+    // type name for this widget
+    static const String WidgetTypeName;             //!< The unique typename of this widget
 
 
-	/*************************************************************************
-		Construction and Destruction
-	*************************************************************************/
-	/*!
-	\brief
-		Constructor for GUISheet windows.
-	*/
-	GUISheet(const String& type, const String& name) : Window(type, name) {}
+    /*************************************************************************
+        Construction and Destruction
+    *************************************************************************/
+    /*!
+    \brief
+        Constructor for GUISheet windows.
+    */
+    GUISheet(const String& type, const String& name) : Window(type, name) {}
 
 
-	/*!
-	\brief
-		Destructor for GUISheet windows.
-	*/
-	virtual ~GUISheet(void) {}
+    /*!
+    \brief
+        Destructor for GUISheet windows.
+    */
+    virtual ~GUISheet(void) {}
 
 
 protected:
-	/*!
-	\brief
-		Perform the actual rendering for this Window.
+    /*!
+    \brief
+        Perform the actual rendering for this Window.
 
-	\param z
-		float value specifying the base Z co-ordinate that should be used when rendering
+    \param z
+        float value specifying the base Z co-ordinate that should be used when rendering
 
-	\return
-		Nothing
-	*/
-	virtual	void	drawSelf(float z) {}
+    \return
+        Nothing
+    */
+    virtual void    drawSelf(float z) {}
 };
 
 
 /*!
 \brief
-	Factory class for producing default windows
+    Factory class for producing default windows
 */
 class GUISheetFactory : public WindowFactory
 {
 public:
-	/*************************************************************************
-		Construction and Destruction
-	*************************************************************************/
-	GUISheetFactory(void) : WindowFactory(GUISheet::WidgetTypeName) { }
-	~GUISheetFactory(void){}
+    /*************************************************************************
+        Construction and Destruction
+    *************************************************************************/
+    GUISheetFactory(void) : WindowFactory(GUISheet::WidgetTypeName) { }
+    ~GUISheetFactory(void){}
 
 
-	/*!
-	\brief
-		Create a new Window object of whatever type this WindowFactory produces.
+    /*!
+    \brief
+        Create a new Window object of whatever type this WindowFactory produces.
 
-	\param name
-		A unique name that is to be assigned to the newly created Window object
+    \param name
+        A unique name that is to be assigned to the newly created Window object
 
-	\return
-		Pointer to the new Window object.
-	*/
-	Window*	createWindow(const String& name)
-	{
-		GUISheet* wnd = new GUISheet(d_type, name);
-		wnd->initialise();
-		wnd->setMaximumSize(Size(1.0f, 1.0f));
-		wnd->setSize(Size(1.0f, 1.0f));
+    \return
+        Pointer to the new Window object.
+    */
+    Window* createWindow(const String& name)
+    {
+        GUISheet* wnd = new GUISheet(d_type, name);
+        wnd->initialise();
+        wnd->setMaximumSize(Size(1.0f, 1.0f));
+        wnd->setSize(Size(1.0f, 1.0f));
 
-		return wnd;
-	}
+        return wnd;
+    }
 
 
-	/*!
-	\brief
-		Destroys the given Window object.
+    /*!
+    \brief
+        Destroys the given Window object.
 
-	\param window
-		Pointer to the Window object to be destroyed.
+    \param window
+        Pointer to the Window object to be destroyed.
 
-	\return
-		Nothing.
-	*/
-	virtual void	destroyWindow(Window* window)	 { if (window->getType() == d_type) delete window; }
+    \return
+        Nothing.
+    */
+    virtual void    destroyWindow(Window* window)    { if (window->getType() == d_type) delete window; }
 };
+
+/*!
+\brief
+    typedef for DefaultWindow, which is the new name for GUISheet.
+*/
+typedef GUISheet DefaultWindow;
 
 
 } // End of  CEGUI namespace section
 
 
-#endif	// end of guard _CEGUIGUISheet_h_
+#endif  // end of guard _CEGUIGUISheet_h_
