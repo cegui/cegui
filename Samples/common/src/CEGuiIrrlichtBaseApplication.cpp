061b7a63613dab1437e0f3a639266b1369c3f68d
Paul Turner <paul@cegui.org.uk>
2005-03-15 10:21:05 +0000

    Linux fixes for fixes made to fix MSVC :-)

--- Samples/common/src/CEGuiIrrlichtBaseApplication.cpp
+++ Samples/common/src/CEGuiIrrlichtBaseApplication.cpp
@@ -21,6 +21,10 @@
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *************************************************************************/
+#ifdef HAVE_CONFIG_H
+#   include "config.h"
+#endif
+
 // this controls conditional compile of file for MSVC
 #include "CEGUIConfig.h"
 #ifdef CEGUI_SAMPLES_USE_IRRLICHT
@@ -111,3 +115,4 @@ bool CEGuiIrrlichtBaseApplication::OnEvent(irr::SEvent event)
 }
 
 #endif
+
