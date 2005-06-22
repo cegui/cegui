b1002e8050c2822ae9d0e252b64fb73cb1a2c2ad
Paul Turner <paul@cegui.org.uk>
2005-06-22 12:50:15 +0000

    Added initial structure for CEGUIFalagardBase module.

---
+++ WidgetSets/Falagard/src/FalModule.cpp
@@ -0,0 +1,81 @@
+/************************************************************************
+    filename:   FalModule.cpp
+    created:    Wed Jun 22 2005
+    author:     Paul D Turner <paul@cegui.org.uk>
+*************************************************************************/
+/*************************************************************************
+    Crazy Eddie's GUI System (http://www.cegui.org.uk)
+    Copyright (C)2004 - 2005 Paul D Turner (paul@cegui.org.uk)
+ 
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+ 
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+ 
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*************************************************************************/
+#include "FalModule.h"
+#include "CEGUIExceptions.h"
+#include "CEGUIWindowFactoryManager.h"
+
+/*************************************************************************
+    Static factory objects
+*************************************************************************/
+
+
+using namespace CEGUI;
+
+struct mapEntry
+{
+    const CEGUI::utf8* d_name;
+    CEGUI::WindowFactory* d_factory;
+};
+
+mapEntry factoriesMap[] =
+{
+    {0,0}
+};
+
+/*************************************************************************
+	Plugin access interface
+*************************************************************************/
+extern "C" void registerFactory(const CEGUI::String& type_name)
+{
+    mapEntry* entry = factoriesMap;
+
+    while (entry->d_name)
+    {
+        if (entry->d_name == type_name)
+        {
+            WindowFactoryManager::getSingleton().addFactory(entry->d_factory);
+            return;
+        }
+
+        ++entry;
+    }
+
+    throw UnknownObjectException((utf8*)"::registerFactory - The window factory for type '" + type_name + "' is not known in this module.");
+}
+
+extern "C" CEGUI::uint registerAllFactories(void)
+{
+    CEGUI::uint count = 0;
+    mapEntry* entry = factoriesMap;
+
+    while (entry->d_name)
+    {
+        WindowFactoryManager::getSingleton().addFactory(entry->d_factory);
+
+        ++entry;
+        ++count;
+    }
+
+    return count;
+}
