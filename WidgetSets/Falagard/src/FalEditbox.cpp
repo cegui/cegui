a74c207863eb172790e557022c2a3a49b2ab0a6d
Tomas Lindquist Olsen <lindquist@users.sourceforge.net>
2005-07-21 15:34:57 +0000

    FalagardEditbox did not detect inherited disabled state when rendering

--- WidgetSets/Falagard/src/FalEditbox.cpp
+++ WidgetSets/Falagard/src/FalEditbox.cpp
@@ -48,7 +48,7 @@ namespace CEGUI
         // get WidgetLookFeel for the assigned look.
         const WidgetLookFeel& wlf = WidgetLookManager::getSingleton().getWidgetLook(d_lookName);
         // try and get imagery for the approprite state.
-        imagery = &wlf.getStateImagery(d_enabled ? (isReadOnly() ? "ReadOnly" : "Enabled") : "Disabled");
+        imagery = &wlf.getStateImagery(isDisabled() ? "Disabled" : (isReadOnly() ? "ReadOnly" : "Enabled"));
 
         // peform the rendering operation for the container.
         imagery->render(*this);
