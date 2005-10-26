3abcc3e769932ac8895f6865f8aa35bbb4e8e71e
Tomas Lindquist Olsen <lindquist@users.sourceforge.net>
2005-10-26 17:35:17 +0000

    Added a customized tolua++ binary. For exception handling support in generated binding code.

---
+++ ScriptingModules/CEGUILua/tolua++bin/toluabind.h
@@ -0,0 +1,8 @@
+/*
+** Lua binding: tolua
+** Generated automatically by tolua++-1.0.7 on Wed Oct 26 19:19:16 2005.
+*/
+
+/* Exported function */
+TOLUA_API int tolua_tolua_open (lua_State* tolua_S);
+
