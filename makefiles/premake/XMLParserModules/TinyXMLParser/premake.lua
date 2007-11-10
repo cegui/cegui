310f4b3b8254daa90399ca049bd26231f3921d2d
Frederico Jeronimo <fjeronimo@users.sourceforge.net>
2007-11-10 01:35:55 +0000

    Exception type, line and file info - See http://www.cegui.org.uk/phpBB2/viewtopic.php?t=2799

--- makefiles/premake/XMLParserModules/TinyXMLParser/premake.lua
+++ makefiles/premake/XMLParserModules/TinyXMLParser/premake.lua
@@ -12,6 +12,12 @@ package.files =
 	matchfiles(pkgdir.."ceguitinyxml/*.cpp"),
 }
 
+-- We need to remove edit-and-continue in VS2003 (7.1) or older versions because 
+-- of the __LINE__ expansion macro bug (see http://support.microsoft.com/kb/199057/en)
+if target=="vs2002" or target=="vs2003" then
+	package.buildflags = { "no-edit-and-continue" }
+end
+
 include(pkgdir)
 
 dependency("CEGUIBase")
