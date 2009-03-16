082b72bd6691c2092c081889311c76ab80c606a1
Paul Turner <paul@cegui.org.uk>
2009-03-16 08:45:19 +0000

    MOD: Rewrote Mac bundle loading to look in 'PlugIns' locations instead of 'Resources' locations.

--- cegui/src/implementations/mac/macPlugins.cpp
+++ cegui/src/implementations/mac/macPlugins.cpp
@@ -1,9 +1,9 @@
 /***************************************************************************
- * This file have been borowed from the Ogre project 
- * @LICENSE CHECK@
+    This file was originally borowed from the Ogre project, though is now
+    largely rewritten.
  ***************************************************************************/
 /***************************************************************************
- *   Copyright (C) 2004 - 2006 Paul D Turner & The CEGUI Development Team
+ *   Copyright (C) 2004 - 2009 Paul D Turner & The CEGUI Development Team
  *
  *   Permission is hereby granted, free of charge, to any person obtaining
  *   a copy of this software and associated documentation files (the
@@ -28,138 +28,160 @@
 #include "macPlugins.h"
 #include "CEGUILogger.h"
 
-int main(void)
-{
-}
-
 namespace CEGUI 
 {
-     
-     CFBundleRef mac_loadExeBundle(const char* name) 
-     {
-          Logger* logger = Logger::getSingletonPtr();
-          
-          if(logger)
-          {
-               logger->logEvent("---- Beginning exe bundle loading ----");
-               logger->logEvent(name);
-          
-               logger->logEvent("Get reference to base bundle", Insane);
-          }
-          CFBundleRef baseBundle = CFBundleGetBundleWithIdentifier(CFSTR("net.sourceforge.crayzedsgui.CEGUIBase"));
+//----------------------------------------------------------------------------//   
+CFStringRef createBundleNameString(const char* name)
+{
+    CFStringRef s = CFStringCreateWithCString(0, name, kCFStringEncodingASCII);
 
-          if(logger) logger->logEvent("Get reference to main bundle", Insane);
-          CFBundleRef mainBundle = CFBundleGetMainBundle();
-          
-          if(logger) logger->logEvent("Create name", Insane);
-          CFStringRef nameRef = CFStringCreateWithCString(0, name, kCFStringEncodingASCII);
-          CFURLRef bundleURL = 0; //URL of bundle to load
-          CFBundleRef bundle = 0; //bundle to load
-          
-          // Cut off .bundle if present.
-          if(logger) logger->logEvent("Check if .bundle suffix is on name", Insane);
-          if(CFStringHasSuffix(nameRef, CFSTR(".bundle"))) 
-          {
-               if(logger) logger->logEvent("Create temporary name reference", Insane);
-               CFStringRef nameTempRef = nameRef;
-               int end = CFStringGetLength(nameTempRef) - CFStringGetLength(CFSTR(".bundle"));
-               nameRef = CFStringCreateWithSubstring(0, nameTempRef, CFRangeMake(0, end));
+    if(!CFStringHasSuffix(s, CFSTR(".bundle"))) 
+    {
+        CFMutableStringRef ms = CFStringCreateMutableCopy(0, 0, s);
+        CFRelease(s);
+        CFStringAppend(ms, CFSTR(".bundle"));
+        s = ms;
+    }
+    
+    return s;
+}
 
-               if(logger) logger->logEvent("Release temporary name reference", Insane);
-               CFRelease(nameTempRef);
-          }
-          
-          // Assume relative to Resources/ directory of application's bundle.
-          if(logger) logger->logEvent("Create bundle URL", Insane);
-          bundleURL = CFBundleCopyResourceURL(mainBundle, nameRef, CFSTR("bundle"), 0);
-          if(bundleURL)
-          {
-               if(logger) logger->logEvent("Create bundle from URL", Insane);
-               bundle = CFBundleCreate(0, bundleURL);
-               
-               if(logger) logger->logEvent("Release bundle URL", Insane);
-               CFRelease(bundleURL);
-          }
-          
-          // Otherwise, try Resources/ directory of CEGUI Framework bundle.
-          if(!bundle) 
-          {
-               if(logger) logger->logEvent("Couldn't get bundle from main bundle reference; try base");
-               bundleURL = CFBundleCopyResourceURL(baseBundle, nameRef, CFSTR("bundle"), 0);
-               if(bundleURL) 
-               {
-                    if(logger) logger->logEvent("Create bundle from URL", Insane);
-                    bundle = CFBundleCreate(0, bundleURL);
-                    
-                    if(logger) logger->logEvent("Release bundle URL", Insane);
-                    CFRelease(bundleURL);
-               }
-          }
-          if(logger) logger->logEvent("Release name reference", Insane);
-          CFRelease(nameRef);
-          
-          if(bundle) 
-          {
-               if(logger) logger->logEvent("Load the bundle executable.", Insane);
-               if(CFBundleLoadExecutable(bundle)) 
-               {
-                    if(logger) logger->logEvent("Bundle loaded successfully.");
-                    return bundle;
-               }
-               else 
-               {
-                    if(logger) logger->logEvent("Bundle loading failed!");
-                    CFRelease(bundle);
-               }
-          }
-          
-          if(logger) logger->logEvent("Failure; return 0", Insane);
-          return 0;
-     }
-     
-     void* mac_getBundleSym(CFBundleRef bundle, const char* name) 
-     {
-          Logger* logger = Logger::getSingletonPtr();
-          
-          if(logger) logger->logEvent("---- Getting bundle symbol ----", Insane);
-          CFStringRef nameRef = CFStringCreateWithCString(0, name, kCFStringEncodingASCII);
-          
-          if(logger) 
-          {
-               logger->logEvent("Find function pointer for name: ", Insane);
-               logger->logEvent(name, Insane);
-          }
-          void* sym = CFBundleGetFunctionPointerForName(bundle, nameRef);
-          
-          if(logger) logger->logEvent("Release bundle name", Insane);
-          CFRelease(nameRef);
-          
-          if(logger) logger->logEvent("---- Done getting bundle symbol ----", Insane);
-          return sym;
-     }
-          
-     // Returns 1 on error, 0 otherwise.
-     bool mac_unloadExeBundle(CFBundleRef bundle) 
-     {
-          Logger* logger = Logger::getSingletonPtr();
-          
-          if(logger) logger->logEvent("---- Beginning exe bundle unloading ----");
-          
-          if(bundle) 
-          {
-               if(logger) logger->logEvent("Bundle unloaded.", Insane);
+//----------------------------------------------------------------------------//
+CFURLRef createFullPluginsURL(CFBundleRef bundle)
+{
+    CFURLRef pluginsURL = 0;
 
-               // No-op, can't unload Obj-C bundles without crashing.
-               return 0;
-          }
-          
-          if(logger) logger->logEvent("---- Ending exe bundle unloading ----");
-          return 1;
-     }
-     
-     const char* mac_errorBundle() 
-     {
-          return "Unknown Error";
-     }
+    CFURLRef baseURL = CFBundleCopyBundleURL(bundle);
+
+    if (baseURL)
+    {
+        CFURLRef pluginsLocation = CFBundleCopyBuiltInPlugInsURL(bundle);
+        
+        if (pluginsLocation)
+        {
+            pluginsURL = CFURLCreateCopyAppendingPathComponent(0, baseURL,
+                                              CFURLGetString(pluginsLocation),
+                                              false);
+            CFRelease(pluginsLocation);
+        }
+        
+        CFRelease(baseURL);
+    }
+    
+    return pluginsURL;
+}
+
+//----------------------------------------------------------------------------//
+CFBundleRef createBundle(CFBundleRef sourceBundle, CFStringRef bundleName)
+{
+    CFBundleRef bundle = 0;
+
+    CFURLRef pluginsURL = createFullPluginsURL(sourceBundle);
+
+    if (pluginsURL)
+    {
+        CFURLRef bundleURL =
+            CFURLCreateCopyAppendingPathComponent(0, pluginsURL,
+                                                  bundleName, false);
+        if (bundleURL)
+        {
+            bundle = CFBundleCreate(0, bundleURL);
+            CFRelease(bundleURL);
+        }
+
+        CFRelease(pluginsURL);
+    }
+
+    return bundle;
+}
+
+//----------------------------------------------------------------------------//
+CFBundleRef mac_loadExeBundle(const char* name) 
+{
+    Logger* logger = Logger::getSingletonPtr();
+
+    if (logger)
+    {
+       logger->logEvent("---- Beginning exe bundle loading ----");
+       logger->logEvent("-- Input bundle name: " + String(name));
+    }
+
+    CFBundleRef bundle = 0;
+
+    CFStringRef pluginName = createBundleNameString(name);
+    
+    if (pluginName)
+    {
+        bundle = createBundle(CFBundleGetMainBundle(), pluginName);
+
+        if (!bundle)
+        {
+            if(logger)
+                logger->logEvent(
+                    "-- Couldn't get bundle from main app bundle reference; "
+                    "trying CEGUIbase instead.");
+
+            bundle =
+                createBundle(CFBundleGetBundleWithIdentifier(
+                                CFSTR("net.sourceforge.crayzedsgui.CEGUIBase")), 
+                             pluginName);
+        }
+
+        if (bundle) 
+        {
+            if (!CFBundleLoadExecutable(bundle))
+            {
+                CFRelease(bundle);
+                bundle = 0;
+            }
+        }
+        
+        CFRelease(pluginName);
+    }
+
+    if (logger)
+    {
+        if (bundle)
+            logger->logEvent("-- Bundle loaded successfully.");
+        else
+            logger->logEvent("-- Bundle loading failed!");
+
+        logger->logEvent("---- Finished exe bundle loading ----");
+    }
+  
+    return bundle;
+}
+
+//----------------------------------------------------------------------------//
+void* mac_getBundleSym(CFBundleRef bundle, const char* name) 
+{
+    CFStringRef nameRef =
+        CFStringCreateWithCString(0, name, kCFStringEncodingASCII);
+    void* sym = CFBundleGetFunctionPointerForName(bundle, nameRef);
+    CFRelease(nameRef);
+
+    return sym;
+}
+
+//----------------------------------------------------------------------------//
+// Returns 1 on error, 0 otherwise.
+bool mac_unloadExeBundle(CFBundleRef bundle) 
+{
+    if (bundle) 
+    {
+        // No-op, can't unload Obj-C bundles without crashing.
+        return 0;
+    }
+    
+    return 1;
+}
+
+//----------------------------------------------------------------------------//
+const char* mac_errorBundle() 
+{
+    return "Unknown Error";
+}
+
+//----------------------------------------------------------------------------//
 
 }
