4d83ea3e97d9d2620d2901adb0bf8dfbd51593e7
Paul Turner <paul@cegui.org.uk>
2009-07-15 13:49:28 +0000

    Fix warnings for MSVC++ Fix issues caused by dodgy implementation of specialised word wrapper class for JustifiedRenderedString (should now compile and work on all systems!).

--- cegui/include/CEGUICentredRenderedString.h
+++ cegui/include/CEGUICentredRenderedString.h
@@ -29,6 +29,12 @@
 #define _CEGUICentredRenderedString_h_
 
 #include "CEGUIFormattedRenderedString.h"
+#include <vector>
+
+#if defined (_MSC_VER)
+#	pragma warning(push)
+#	pragma warning(disable : 4251)
+#endif
 
 // Start of CEGUI namespace section
 namespace CEGUI
@@ -57,5 +63,9 @@ protected:
 };
 
 } // End of  CEGUI namespace section
+
+#if defined (_MSC_VER)
+#	pragma warning(pop)
+#endif
 
 #endif // end of guard _CEGUICentredRenderedString_h_
--- cegui/include/CEGUIRightAlignedRenderedString.h
+++ cegui/include/CEGUIRightAlignedRenderedString.h
@@ -31,6 +31,11 @@
 #include "CEGUIFormattedRenderedString.h"
 #include <vector>
 
+#if defined (_MSC_VER)
+#	pragma warning(push)
+#	pragma warning(disable : 4251)
+#endif
+
 // Start of CEGUI namespace section
 namespace CEGUI
 {
@@ -58,5 +63,9 @@ protected:
 };
     
 } // End of  CEGUI namespace section
+
+#if defined (_MSC_VER)
+#	pragma warning(pop)
+#endif
 
 #endif // end of guard _CEGUIRightAlignedRenderedString_h_
