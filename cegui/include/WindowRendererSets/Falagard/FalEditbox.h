e0dd2bcdc8155bb01aa75751248c52ca6a468d46
Paul Turner <paul@cegui.org.uk>
2009-07-29 10:19:39 +0000

    Reformat Falagard Editbox renderer files.

--- cegui/include/WindowRendererSets/Falagard/FalEditbox.h
+++ cegui/include/WindowRendererSets/Falagard/FalEditbox.h
@@ -4,7 +4,7 @@
     author:     Paul D Turner <paul@cegui.org.uk>
 *************************************************************************/
 /***************************************************************************
- *   Copyright (C) 2004 - 2006 Paul D Turner & The CEGUI Development Team
+ *   Copyright (C) 2004 - 2009 Paul D Turner & The CEGUI Development Team
  *
  *   Permission is hereby granted, free of charge, to any person obtaining
  *   a copy of this software and associated documentation files (the
@@ -34,81 +34,93 @@
 // Start of CEGUI namespace section
 namespace CEGUI
 {
+/*!
+\brief
+    Editbox class for the FalagardBase module.
+
+    This class requires LookNFeel to be assigned.  The LookNFeel should provide
+    the following:
+
+    States:
+        - Enabled: Rendering for when the editbox is in enabled and is in
+                   read-write mode.
+        - ReadOnly: Rendering for when the editbox is in enabled and is in
+                    read-only mode.
+        - Disabled: Rendering for when the editbox is disabled.
+        - ActiveSelection: additional state rendered for text selection
+                           (the imagery in this section is rendered within the
+                           selection area.)
+        - InactiveSelection: additional state rendered for text selection
+                             (the imagery in this section is rendered within the
+                             selection area.)
+
+    NamedAreas:
+        - TextArea: area where text, selection, and carat imagery will appear.
+
+    PropertyDefinitions (optional, defaults will be black):
+        - NormalTextColour: property that accesses a colour value to be used to
+                            render normal unselected text.
+        - SelectedTextColour: property that accesses a colour value to be used
+                              to render selected text.
+
+    Imagery Sections:
+        - Carat
+*/
+class FALAGARDBASE_API FalagardEditbox : public EditboxWindowRenderer
+{
+public:
+    //! type name for this widget.
+    static const utf8 TypeName[];
+    //! Name of property to access for unselected text colour.
+    static const String UnselectedTextColourPropertyName;
+    //! Name of property to access for selected text colour.
+    static const String SelectedTextColourPropertyName;
+
     /*!
     \brief
-        Editbox class for the FalagardBase module.
-
-        This class requires LookNFeel to be assigned.  The LookNFeel should provide the following:
-
-        States:
-            - Enabled    - Rendering for when the editbox is in enabled and is in read-write mode.
-            - ReadOnly  - Rendering for when the editbox is in enabled and is in read-only mode.
-            - Disabled  - Rendering for when the editbox is disabled.
-            - ActiveSelection   - additional state rendered for text selection (the imagery in this section is rendered within the selection area.)
-            - InactiveSelection - additional state rendered for text selection (the imagery in this section is rendered within the selection area.)
-
-        NamedAreas:
-            TextArea    - area where text, selection, and carat imagery will appear.
-
-        PropertyDefinitions (optional, defaults will be black):
-            - NormalTextColour   - property that accesses a colour value to be used to render normal unselected text.
-            - SelectedTextColour - property that accesses a colour value to be used to render selected text.
-
-        Imagery Sections:
-            - Carat
+        Constructor
     */
-    class FALAGARDBASE_API FalagardEditbox : public EditboxWindowRenderer
-    {
-    public:
-        static const utf8   TypeName[];       //!< type name for this widget.
-        static const String UnselectedTextColourPropertyName;   //!< Name of property to access for unselected text colour.
-        static const String SelectedTextColourPropertyName;     //!< Name of property to access for selected text colour.
+    FalagardEditbox(const String& type);
 
-        /*!
-        \brief
-            Constructor
-        */
-        FalagardEditbox(const String& type);
+    /*!
+    \brief
+        return the colour to be used for rendering Editbox text oustside of the
+        selected region.
 
-        /*!
-        \brief
-            return the colour to be used for rendering Editbox text oustside of the
-            selected region.
+    \return
+        colour value describing the colour to be used.
+    */
+    colour getUnselectedTextColour() const;
 
-        \return
-            colour value describing the colour to be used.
-        */
-        colour getUnselectedTextColour() const;
+    /*!
+    \brief
+        return the colour to be used for rendering Editbox text falling within
+        the selected region.
 
-        /*!
-        \brief
-            return the colour to be used for rendering Editbox text falling within
-            the selected region.
+    \return
+        colour value describing the colour to be used.
+    */
+    colour getSelectedTextColour() const;
 
-        \return
-            colour value describing the colour to be used.
-        */
-        colour getSelectedTextColour() const;
+    /*!
+    \brief
+        Return a colour object fetched from the named property if it exists,
+        else a default colour (black).
 
-        /*!
-        \brief
-            Return a colour object fetched from the named property if it exists,
-            else a default colour (black).
+    \param propertyName
+        String object holding the name of the property to be accessed if it
+        exists.
+    */
+    colour getOptionalPropertyColour(const String& propertyName) const;
 
-        \param propertyName
-            String object holding the name of the property to be accessed if it
-            exists.
-        */
-        colour getOptionalPropertyColour(const String& propertyName) const;
+    void render();
 
-        void render();
+    // overridden from EditboxWindowRenderer base class.
+    size_t getTextIndexFromPosition(const Point& pt) const;
 
-        // overridden from EditboxWindowRenderer base class.
-        size_t getTextIndexFromPosition(const Point& pt) const;
-
-        // rendering internal vars
-        float	d_lastTextOffset;		//!< x rendering offset used last time we drew the widget.
-    };
+    //! x rendering offset used last time we drew the widget.
+    float d_lastTextOffset;
+};
 
 } // End of  CEGUI namespace section
 
