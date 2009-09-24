3f4b303e2c671c5ca676c253375bd969eeacaf49
Paul Turner <paul@cegui.org.uk>
2009-09-24 10:55:40 +0000

    FIX: Issue of MultiLineEditbox not being initialised correctly (no initial terminating \n being set).  See: http://www.cegui.org.uk/mantis/view.php?id=304

--- cegui/include/elements/CEGUIMultiLineEditbox.h
+++ cegui/include/elements/CEGUIMultiLineEditbox.h
@@ -69,6 +69,10 @@ public:
         Rect object describing the area of the Window to be used for rendering text.
     */
     virtual Rect getTextRenderArea(void) const = 0;
+
+protected:
+    // base class overrides
+    void onLookNFeelAssigned();
 };
 
 /*!
