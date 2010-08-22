e57d1621135975abcf6b104ae17c4d12c21dd20f
Paul Turner <paul@cegui.org.uk>
2010-08-22 16:42:29 +0000

    ADD: explicit copy constructors for some of the basic data types; to aid in binding other languages :)  Thanks to Kulik.

--- cegui/include/CEGUISize.h
+++ cegui/include/CEGUISize.h
@@ -45,6 +45,7 @@ class CEGUIEXPORT Size
 public:
 	Size(void) {}
 	Size(float width, float height) : d_width(width), d_height(height) {}
+	Size(const Size& v): d_width(v.d_width), d_height(v.d_height) {}
 
 	bool operator==(const Size& other) const;
 	bool operator!=(const Size& other) const;
