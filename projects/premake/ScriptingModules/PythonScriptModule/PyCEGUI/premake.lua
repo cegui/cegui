6629d037683841668e23b201897a71f036bf14f9
Paul Turner <paul@cegui.org.uk>
2010-11-10 21:25:13 +0000

    First working version premake build for the python extension modules (dynamic builds only at the moment).

--- projects/premake/ScriptingModules/PythonScriptModule/PyCEGUI/premake.lua
+++ projects/premake/ScriptingModules/PythonScriptModule/PyCEGUI/premake.lua
@@ -4,10 +4,21 @@
 
 cegui_dynamic("PyCEGUI")
 
+include(rootdir.."cegui/src/ScriptingModules/PythonScriptModule/bindings")
+include(rootdir.."cegui/src/ScriptingModules/PythonScriptModule/bindings/output/CEGUI")
+
 package.files =
 {
 	matchfiles(rootdir.."cegui/src/ScriptingModules/PythonScriptModule/bindings/output/CEGUI/*.cpp"),
 	matchfiles(rootdir.."cegui/src/ScriptingModules/PythonScriptModule/bindings/output/CEGUI/*.hpp"),
 }
+
+if PYTHON_PATHS then
+    add_sdk_paths(PYTHON_PATHS)
+end
+
+if BOOST_PYTHON_PATHS then
+    add_sdk_paths(BOOST_PYTHON_PATHS)
+end
 
 dependency("CEGUIBase")
--- projects/premake/ScriptingModules/PythonScriptModule/PyCEGUIOpenGLRenderer/premake.lua
+++ projects/premake/ScriptingModules/PythonScriptModule/PyCEGUIOpenGLRenderer/premake.lua
@@ -4,11 +4,21 @@
 
 cegui_dynamic("PyCEGUIOpenGLRenderer")
 
+include(rootdir.."cegui/src/ScriptingModules/PythonScriptModule/bindings")
+
 package.files =
 {
 	matchfiles(rootdir.."cegui/src/ScriptingModules/PythonScriptModule/bindings/output/CEGUIOpenGLRenderer/*.cpp"),
 	matchfiles(rootdir.."cegui/src/ScriptingModules/PythonScriptModule/bindings/output/CEGUIOpenGLRenderer/*.hpp"),
 }
+
+if PYTHON_PATHS then
+    add_sdk_paths(PYTHON_PATHS)
+end
+
+if BOOST_PYTHON_PATHS then
+    add_sdk_paths(BOOST_PYTHON_PATHS)
+end
 
 dependency("CEGUIBase")
 dependency("CEGUIOpenGLRenderer")
