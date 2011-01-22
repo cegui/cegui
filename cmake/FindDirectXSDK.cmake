d1552953e6f88c2c60ed11ed3a26c4bebd31cfc7
Paul Turner <paul@cegui.org.uk>
2011-01-22 19:14:25 +0000

    Add initial version of cmake related files + some other bits :-p
    ********************************************************************************
                              I M P O R T A N T
    ********************************************************************************
    
    This commit does not represent anything 'finished' and is very much a work in
    progress.  It's okay to try it out, but not to whine, patch, or even give much
    in the way of feedback.  The sole reason this is being committed is in order
    to more easily keep things from getting too out dated as compared with current
    trunk.
    
    This said, the current status is considered to be:
    
    All builds
    ==========
    Static building is at the moment totally untested.
    
    
    Linux & similar systems
    =======================
    Everything is about 99% completed.  Known exceptions are the missing .pc files
    and the fact that the python modules are named incorrectly and get installed in
    the wrong place :)
    
    
    Windows / MSVC++
    ================
    Again, things are largely complete.  Everything will build ok.  There's probably
    some work to do for detecting certain optional libs, and also the python modules
    have the incorrect file extension.
    
    
    Apple Mac OS X / Xcode
    ======================
    Things are moving in a slightly different direction here, since I'm moving away
    from eveything being a framework - which was total overkill - to mostly using
    the more basic .dylib files.  Due to this, a new dependency pack is required but
    is not available at this time (you can rework the existing dependency binaries to
    be non framework based if you know what you are doing).  In addition, while
    everything will basically build, there is still some work to do: for example the
    sample app bundles do not yet have the dependency files copied / symlinked into
    the appropriate places (though if you do it manually, you can get them to run).
    
    ********************************************************************************

---
+++ cmake/FindDirectXSDK.cmake
@@ -0,0 +1,41 @@
+################################################################################
+# Custom cmake module for CEGUI to find details about the DX SDK
+################################################################################
+include(FindPackageHandleStandardArgs)
+
+if (CMAKE_SIZEOF_VOID_P EQUAL 4)
+    set (DIRECTXSDK_ARCH x86)
+else()
+    set (DIRECTXSDK_ARCH x64)
+endif()
+
+find_path(DIRECTXSDK_H_PATH NAMES d3d10.h PATHS ENV DXSDK_DIR PATH_SUFFIXES Include )
+find_path(DIRECTXSDK_LIB_PATH NAMES d3dx10d.lib PATHS ENV DXSDK_DIR PATH_SUFFIXES "Lib/${DIRECTXSDK_ARCH}")
+
+# now test for the specific d3d lib versions and set MAX_D3D accordingly
+if (EXISTS "${DIRECTXSDK_LIB_PATH}/d3d11.lib")
+    set (DIRECTXSDK_MAX_D3D 11)
+elseif (EXISTS "${DIRECTXSDK_LIB_PATH}/d3d10.lib")
+    set (DIRECTXSDK_MAX_D3D 10)
+elseif (EXISTS "${DIRECTXSDK_LIB_PATH}/d39.lib")
+    set (DIRECTXSDK_MAX_D3D 9)
+else()
+    set (DIRECTXSDK_MAX_D3D)
+endif()
+
+find_package_handle_standard_args(DIRECTXSDK DEFAULT_MSG DIRECTXSDK_LIB_PATH DIRECTXSDK_H_PATH DIRECTXSDK_MAX_D3D)
+
+mark_as_advanced(DIRECTXSDK_H_PATH DIRECTXSDK_LIB_PATH DIRECTXSDK_ARCH)
+
+# set up output vars
+if (DIRECTXSDK_FOUND)
+    set (DIRECTXSDK_INCLUDE_DIR ${DIRECTXSDK_H_PATH})
+    set (DIRECTXSDK_LIBRARY_DIR ${DIRECTXSDK_LIB_PATH})
+else()
+    set (DIRECTXSDK_INCLUDE_DIR)
+    set (DIRECTXSDK_LIBRARY_DIR)
+    set (DIRECTXSDK_MAX_D3D)
+endif()
+
+
+
