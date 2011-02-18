b76db82aff183231a59553d7845191d6b9d60ed3
Paul Turner <paul@cegui.org.uk>
2011-02-18 09:28:39 +0000

    FIX: Some include guards were incorrect.

--- cegui/include/CEGUIRenderEffectFactory.h
+++ cegui/include/CEGUIRenderEffectFactory.h
@@ -25,7 +25,7 @@
  *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  *   OTHER DEALINGS IN THE SOFTWARE.
  ***************************************************************************/
-#ifndef _CEGUIRenderEffectFactory_h
+#ifndef _CEGUIRenderEffectFactory_h_
 #define _CEGUIRenderEffectFactory_h_
 
 // Start of CEGUI namespace section
