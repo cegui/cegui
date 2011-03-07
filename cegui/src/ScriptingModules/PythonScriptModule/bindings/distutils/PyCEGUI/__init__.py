cf377425aa9a8af31410eecee5a3a1f5c7cd3b39
Martin Preisler <Martin Preisler@localhost>
2011-03-07 18:16:42 +0100

    FIX: distutils under Windows FIX: removed the debug print in the fake PyCEGUI module (I forgot it there)

--- cegui/src/ScriptingModules/PythonScriptModule/bindings/distutils/PyCEGUI/__init__.py
+++ cegui/src/ScriptingModules/PythonScriptModule/bindings/distutils/PyCEGUI/__init__.py
@@ -7,7 +7,7 @@ def get_my_path():
     return os.path.dirname(str(fake).split()[3][1:])
 
 libpath = os.path.abspath(get_my_path())
-print "libpath =", libpath
+#print "libpath =", libpath
 os.environ['PATH'] = libpath + ";" + os.environ['PATH']
 
 from PyCEGUI import *
