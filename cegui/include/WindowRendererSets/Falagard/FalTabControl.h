2b789fe33852b3a1014fce69f09a585d436ef286
Hanmac <Hanmac@localhost>
2011-08-28 22:40:07 +0200

    MOD: Migrated properties of FalTabControl to FalagardProperty

--- cegui/include/WindowRendererSets/Falagard/FalTabControl.h
+++ cegui/include/WindowRendererSets/Falagard/FalTabControl.h
@@ -30,7 +30,7 @@
 
 #include "FalModule.h"
 #include "../../elements/CEGUITabControl.h"
-#include "FalTabControlProperties.h"
+#include "../../CEGUITplWindowRendererProperty.h"
 
 #if defined(_MSC_VER)
 #	pragma warning(push)
@@ -85,9 +85,6 @@ namespace CEGUI
 
         // data fields
         String  d_tabButtonType;
-
-        // properties
-        static FalagardTabControlProperties::TabButtonType d_tabButtonTypeProperty;
     };
 
 } // End of  CEGUI namespace section
