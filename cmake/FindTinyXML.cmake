56ceaaf267adbf4beda0c714e7092ba5b7d0c768
Paul Turner <paul@cegui.org.uk>
2011-10-01 10:23:42 +0100

    FIX: TinyXML API changed with version 2.6.  This fixes CEGUI to work with either API (which is now detected by cmake).  See: http://www.cegui.org.uk/mantis/view.php?id=458

--- cmake/FindTinyXML.cmake
+++ cmake/FindTinyXML.cmake
@@ -2,6 +2,7 @@
 # Custom cmake module for CEGUI to find tinyxml
 ################################################################################
 include(FindPackageHandleStandardArgs)
+include(CheckCXXSourceCompiles)
 
 find_path(TINYXML_H_PATH NAMES tinyxml.h)
 find_library(TINYXML_LIB NAMES tinyxml libtinyxml)
@@ -10,9 +11,20 @@ mark_as_advanced(TINYXML_H_PATH TINYXML_LIB TINYXML_LIB_DBG)
 
 find_package_handle_standard_args(TINYXML DEFAULT_MSG TINYXML_LIB TINYXML_H_PATH)
 
-
-# set up output vars
 if (TINYXML_FOUND)
+    # what API version do we have here?
+    set(CMAKE_REQUIRED_INCLUDES ${TINYXML_H_PATH})
+    set(CMAKE_REQUIRED_LIBRARIES ${TINYXML_LIB})
+    check_cxx_source_compiles("
+    #include <tinyxml.h>
+    int main() {
+        int i = TiXmlElement::TINYXML_ELEMENT;
+        return 0;
+    }"
+    
+    TINYXML_API_TEST)
+
+    set (CEGUI_TINYXML_HAS_2_6_API ${TINYXML_API_TEST})
     set (TINYXML_INCLUDE_DIR ${TINYXML_H_PATH})
     set (TINYXML_LIBRARIES ${TINYXML_LIB})
     if (TINYXML_LIB_DBG)
