81cbc312f46d358a5ac346f3f32c39dc769b4e4a
Martin Preisler <preisler.m@gmail.com>
2011-10-28 18:28:13 +0200

    Element relative sizing test case

--- tests/Element.cpp
+++ tests/Element.cpp
@@ -33,9 +33,27 @@
 
 BOOST_AUTO_TEST_SUITE(Element)
 
-BOOST_AUTO_TEST_CASE(UnifiedDimensions)
+BOOST_AUTO_TEST_CASE(RelativeSizing)
 {
+    // in this test, everything is top left aligned
     
+    CEGUI::Element* root = new CEGUI::Element();
+    root->setSize(CEGUI::USize(100.0f * CEGUI::UDim::px(), 100 * CEGUI::UDim::px()));
+    CEGUI::Element* child = new CEGUI::Element();
+    child->setSize(CEGUI::USize(50.0f * CEGUI::UDim::percent(), 25.0f * CEGUI::UDim::percent()));
+    root->addChild(child);
+    
+    BOOST_CHECK_EQUAL(root->getPixelSize(), CEGUI::Sizef(100.0f, 100.0f));
+    BOOST_CHECK_EQUAL(child->getPixelSize(), CEGUI::Sizef(50.0f, 25.0f));
+    
+    CEGUI::Element* innerChild = new CEGUI::Element();
+    child->addChild(innerChild);
+    innerChild->setSize(CEGUI::USize(200.0f * CEGUI::UDim::percent(), 100.0f * CEGUI::UDim::percent()));
+    BOOST_CHECK_EQUAL(innerChild->getPixelSize(), CEGUI::Sizef(100.0f, 25.0f));
+    
+    delete innerChild;
+    delete child;
+    delete root;
 }
 
 BOOST_AUTO_TEST_SUITE_END()
