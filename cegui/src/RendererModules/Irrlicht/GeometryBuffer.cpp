95791ec0a5f3aa2d319084ee1bc48e6691eefe8e
Paul Turner <paul@cegui.org.uk>
2011-10-30 19:23:54 +0000

    Fix the remaining renderer modules to not use the removed PixelAligned macro.

--- cegui/src/RendererModules/Irrlicht/GeometryBuffer.cpp
+++ cegui/src/RendererModules/Irrlicht/GeometryBuffer.cpp
@@ -164,10 +164,10 @@ void IrrlichtGeometryBuffer::setPivot(const Vector3f& p)
 //----------------------------------------------------------------------------//
 void IrrlichtGeometryBuffer::setClippingRegion(const Rectf& region)
 {
-    d_clipRect.top(ceguimax(0.0f, PixelAligned(region.top())));
-    d_clipRect.bottom(ceguimax(0.0f, PixelAligned(region.bottom())));
-    d_clipRect.left(ceguimax(0.0f, PixelAligned(region.left())));
-    d_clipRect.right(ceguimax(0.0f, PixelAligned(region.right())));
+    d_clipRect.top(ceguimax(0.0f, region.top()));
+    d_clipRect.bottom(ceguimax(0.0f, region.bottom()));
+    d_clipRect.left(ceguimax(0.0f, region.left()));
+    d_clipRect.right(ceguimax(0.0f, region.right()));
 }
 
 //----------------------------------------------------------------------------//
