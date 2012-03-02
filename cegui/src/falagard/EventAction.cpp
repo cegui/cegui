28ea5aaeedfcba44f066c0765cfbeb353d642f98
Paul Turner <paul@cegui.org.uk>
2012-03-02 12:12:55 +0000

    ADD: Initial stab at an implementation for EventAction class (not complete yet, need to add cleanup related parts)

---
+++ cegui/src/falagard/EventAction.cpp
@@ -0,0 +1,99 @@
+/***********************************************************************
+    filename:   EventAction.cpp
+    created:    Fri Mar 02 2012
+    author:     Paul D Turner <paul@cegui.org.uk>
+*************************************************************************/
+/***************************************************************************
+ *   Copyright (C) 2004 - 2012 Paul D Turner & The CEGUI Development Team
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining
+ *   a copy of this software and associated documentation files (the
+ *   "Software"), to deal in the Software without restriction, including
+ *   without limitation the rights to use, copy, modify, merge, publish,
+ *   distribute, sublicense, and/or sell copies of the Software, and to
+ *   permit persons to whom the Software is furnished to do so, subject to
+ *   the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be
+ *   included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *   IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *   OTHER DEALINGS IN THE SOFTWARE.
+ ***************************************************************************/
+#include "CEGUI/falagard/EventAction.h"
+#include "CEGUI/Window.h"
+
+namespace CEGUI
+{
+//----------------------------------------------------------------------------//
+// Functor used to subscribe to events - this is where the magic happens!
+struct EventActionFunctor
+{
+    EventActionFunctor(Window& window, ChildEventAction action) :
+        window(window),
+        action(action)
+    {}
+
+    bool operator()(const EventArgs& args) const
+    {
+        switch (action)
+        {
+        case CEA_REDRAW:
+            window.invalidate(false);
+            return true;
+
+        case CEA_LAYOUT:
+            window.performChildWindowLayout();
+            return true;
+
+        default:
+            CEGUI_THROW(InvalidRequestException(
+                "EventActionFunctor: Encountered invalid action."));
+        }
+
+        return false;
+    }
+
+    Window& window;
+    ChildEventAction action;
+};
+
+//----------------------------------------------------------------------------//
+EventAction::EventAction(const String& event_name,
+                         ChildEventAction action) :
+    d_eventName(event_name),
+    d_action(action)
+{
+}
+
+//----------------------------------------------------------------------------//
+EventAction::~EventAction()
+{
+}
+
+//----------------------------------------------------------------------------//
+void EventAction::initialiseWidget(Window& widget) const
+{
+    Window* parent = widget.getParent();
+
+    if (!parent)
+        CEGUI_THROW(InvalidRequestException("EventAction::initialiseWidget:"
+            "EvenAction can only be initialised on child widgets."));
+
+    widget.subscribeEvent(d_eventName, EventActionFunctor(*parent, d_action));
+}
+
+//----------------------------------------------------------------------------//
+void EventAction::cleanupWidget(Window& widget) const
+{
+}
+
+//----------------------------------------------------------------------------//
+
+}
+
