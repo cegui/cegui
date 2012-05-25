8e5496f24d09935eaecb1d20831c1ac3f228bfe1
Paul Turner <paul@cegui.org.uk>
2012-05-25 13:09:01 +0100

    MOD: Modify the DevIL ImageCodec implementation so that compressed DDS data can be passed to CEGUI::Texture implementations that support that compression. MOD: Dropped dependency on ILUT library.

--- cegui/src/ImageCodecModules/DevIL/ImageCodec.cpp
+++ cegui/src/ImageCodecModules/DevIL/ImageCodec.cpp
@@ -6,7 +6,7 @@
 	purpose:	This codec provide DevIL based image loading 
 *************************************************************************/
 /***************************************************************************
- *   Copyright (C) 2004 - 2006 Paul D Turner & The CEGUI Development Team
+ *   Copyright (C) 2004 - 2012 Paul D Turner & The CEGUI Development Team
  *
  *   Permission is hereby granted, free of charge, to any person obtaining
  *   a copy of this software and associated documentation files (the
@@ -29,26 +29,30 @@
  ***************************************************************************/
 #include "CEGUI/ImageCodecModules/DevIL/ImageCodec.h"
 #include "CEGUI/Size.h"
+#include "CEGUI/Exceptions.h"
 #include <IL/il.h>
-#include <IL/ilu.h> 
 #include <string.h>
 
 // Start of CEGUI namespace section
 namespace CEGUI
 {
+//----------------------------------------------------------------------------//
+// prototypes for internal helper functions
+uchar* getCompressedPixelData(ILenum dxtc_fmt);
+
+//----------------------------------------------------------------------------//
 DevILImageCodec::DevILImageCodec()
     : ImageCodec("DevILImageCodec - Official DevIL based image codec")
 {
-    // init DevIL libs we use
     ilInit();
-    iluInit();
 }
 
+//----------------------------------------------------------------------------//
 DevILImageCodec::~DevILImageCodec()
 {    
-
 }
 
+//----------------------------------------------------------------------------//
 Texture* DevILImageCodec::load(const RawDataContainer& data, Texture* result)
 {
     ilPushAttrib(IL_ORIGIN_SET);
@@ -59,37 +63,62 @@ Texture* DevILImageCodec::load(const RawDataContainer& data, Texture* result)
     ilGenImages(1, &imgName);
     ilBindImage(imgName);
 
+    ilSetInteger(IL_KEEP_DXTC_DATA, IL_TRUE);
+
     if (IL_FALSE != ilLoadL(IL_TYPE_UNKNOWN,
                             static_cast<const void*>(data.getDataPtr()),
                             data.getSize()))
     {
-        // get details about size of loaded image
-        ILinfo imgInfo;
-        memset(&imgInfo, 0, sizeof(ILinfo));
-        iluGetImageInfo(&imgInfo);
-        // set dimensions of texture
-        size_t width = imgInfo.Width;
-        size_t height = imgInfo.Height;
-        // allocate temp buffer to receive image data
-        uchar* tmpBuff = new uchar[width * height * 4];
+        const size_t width = ilGetInteger(IL_IMAGE_WIDTH);
+        const size_t height = ilGetInteger(IL_IMAGE_HEIGHT);
 
-        // get image data in required format
         Texture::PixelFormat cefmt;
-        ILenum ilfmt;
-        switch (imgInfo.Format)
+        uchar* pixel_data;
+
+        switch (const ILenum dxtc_fmt = ilGetInteger(IL_DXTC_DATA_FORMAT))
         {
-        case IL_RGBA:
-        case IL_BGRA:
-            ilfmt = IL_RGBA;
-            cefmt = Texture::PF_RGBA;
+        case IL_DXT1:
+            pixel_data = getCompressedPixelData(dxtc_fmt);
+            cefmt = Texture::PF_RGBA_DXT1;
             break;
+
+        case IL_DXT3:
+            pixel_data = getCompressedPixelData(dxtc_fmt);
+            cefmt = Texture::PF_RGBA_DXT3;
+            break;
+
+        case IL_DXT5:
+            pixel_data = getCompressedPixelData(dxtc_fmt);
+            cefmt = Texture::PF_RGBA_DXT5;
+            break;
+
+        case IL_DXT_NO_COMP:
+            // get image data in required format
+            ILenum ilfmt;
+            switch (ilGetInteger(IL_IMAGE_FORMAT))
+            {
+            case IL_RGBA:
+            case IL_BGRA:
+                ilfmt = IL_RGBA;
+                cefmt = Texture::PF_RGBA;
+                break;
+
+            default:
+                ilfmt = IL_RGB;
+                cefmt = Texture::PF_RGB;
+                break;
+            };
+
+            // allocate temp buffer to receive image data
+            pixel_data = new uchar[width * height * 4];
+            ilCopyPixels(0, 0, 0, width, height, 1, ilfmt, IL_UNSIGNED_BYTE,
+                         static_cast<void*>(pixel_data));
+            break;
+
         default:
-            ilfmt = IL_RGB;
-            cefmt = Texture::PF_RGB;
-            break;
-        };
-        ilCopyPixels(0, 0, 0, width, height, 1, ilfmt, IL_UNSIGNED_BYTE,
-                     static_cast<void*>(tmpBuff));
+            CEGUI_THROW(InvalidRequestException(
+                "[DevILImageCodec] Unsupported DXTC data format returned."));
+        }
 
         // delete DevIL image
         ilDeleteImages(1, &imgName);
@@ -98,16 +127,16 @@ Texture* DevILImageCodec::load(const RawDataContainer& data, Texture* result)
         // create cegui texture
         CEGUI_TRY
         {
-            result->loadFromMemory(tmpBuff, Sizef(width, height), cefmt);
+            result->loadFromMemory(pixel_data, Sizef(width, height), cefmt);
         }
         CEGUI_CATCH(...)
         {
-            delete [] tmpBuff;
+            delete[] pixel_data;
             CEGUI_RETHROW;
         }
 
         // free temp buffer
-        delete [] tmpBuff;
+        delete[] pixel_data;
 
         return result;
     }
@@ -121,8 +150,17 @@ Texture* DevILImageCodec::load(const RawDataContainer& data, Texture* result)
     }
 }
 
+//----------------------------------------------------------------------------//
+uchar* getCompressedPixelData(ILenum dxtc_fmt)
+{
+    ILuint data_size = ilGetDXTCData(0, 0, dxtc_fmt);
+    uchar* pixel_data = new uchar[data_size];
+    ilGetDXTCData(pixel_data, data_size, dxtc_fmt);
+
+    return pixel_data;
+}
+
+//----------------------------------------------------------------------------//
 
 } // End of CEGUI namespace section 
-
-
   
