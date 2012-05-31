eb53f9e1c24aa4ce72159b20d49c1fa095367f3f
Paul Turner <paul@cegui.org.uk>
2012-05-31 20:17:25 +0100

    Reformat / update DefaultLogger files to more closely comply with current coding standards.

--- cegui/include/CEGUI/DefaultLogger.h
+++ cegui/include/CEGUI/DefaultLogger.h
@@ -1,12 +1,12 @@
 /***********************************************************************
-	filename: 	CEGUIDefaultLogger.h
-	created:	25/1/2006
-	author:		Andrew Zabolotny
-	
-	purpose:	Defines interface for the default Logger implementation
+    filename:   CEGUIDefaultLogger.h
+    created:    25/1/2006
+    author:     Andrew Zabolotny
+    
+    purpose:    Defines interface for the default Logger implementation
 *************************************************************************/
 /***************************************************************************
- *   Copyright (C) 2004 - 2006 Paul D Turner & The CEGUI Development Team
+ *   Copyright (C) 2004 - 2012 Paul D Turner & The CEGUI Development Team
  *
  *   Permission is hereby granted, free of charge, to any person obtaining
  *   a copy of this software and associated documentation files (the
@@ -32,21 +32,17 @@
 
 #include "CEGUI/Logger.h"
 
-
 #if defined(_MSC_VER)
-#	pragma warning(push)
-#	pragma warning(disable : 4275)
-#	pragma warning(disable : 4251)
+#   pragma warning(push)
+#   pragma warning(disable : 4275)
+#   pragma warning(disable : 4251)
 #endif
 
-
-// Start of CEGUI namespace section
 namespace CEGUI
 {
-
 /*!
 \brief
-	Default implementation for the Logger class.
+    Default implementation for the Logger class.
     If you want to redirect CEGUI logs to some place other than a text file,
     implement your own Logger implementation and create a object of the
     Logger type before creating the CEGUI::System singleton.
@@ -54,69 +50,33 @@ namespace CEGUI
 class CEGUIEXPORT DefaultLogger : public Logger
 {
 public:
-	/*!
-	\brief
-		Constructor for DefaultLogger object.
-	*/
-	DefaultLogger(void);
+    DefaultLogger(void);
+    ~DefaultLogger(void);
 
-	/*!
-	\brief Destructor for DefaultLogger object.
-	*/
-	virtual ~DefaultLogger(void);
-
-	
-	/*!
-	\brief
-		Add an event to the log.
-
-	\param message
-		String object containing the message to be added to the event log.
-
-	\param level
-		LoggingLevel for this message.  If \a level is greater than the current set logging level, the message is not logged.
-
-	\return
-		Nothing
-	*/
-	virtual void logEvent(const String& message, LoggingLevel level = Standard);
-
-    /*!
-    \brief
-        Set the name of the log file where all subsequent log entries should be written.
-
-    \note
-        When this is called, and the log file is created, any cached log entries are
-        flushed to the log file.
-
-    \param filename
-        Name of the file to put log messages.
-
-    \param append
-        - true if events should be added to the end of the current file.
-        - false if the current contents of the file should be discarded.
-     */
-    virtual void setLogFilename(const String& filename, bool append = false);
+    // overridden from Logger
+    void logEvent(const String& message, LoggingLevel level = Standard);
+    void setLogFilename(const String& filename, bool append = false);
 
 protected:
-    /*************************************************************************
-		Implementation Data
-	*************************************************************************/
-	std::ofstream	d_ostream;		//!< Stream used to implement the logger
-    typedef std::pair<String, LoggingLevel> CacheItem;
+    //! Stream used to implement the logger
+    std::ofstream d_ostream;
+    //! Used to build log entry strings. 
+    std::ostringstream d_workstream;
 
+    typedef std::pair<String, LoggingLevel> CacheItem;
     typedef std::vector<CacheItem
         CEGUI_VECTOR_ALLOC(CacheItem)> Cache;
-
-    Cache d_cache;                  //!< Used to cache log entries before log file is created.
-    std::ostringstream d_workstream;//!< Used to build log entry strings. 
-    bool d_caching;                 //!< true while log entries are beign cached (prior to logfile creation)
+    //! Used to cache log entries before log file is created. 
+    Cache d_cache;
+    //! true while log entries are beign cached (prior to logfile creation)
+    bool d_caching;
 };
 
-} // End of  CEGUI namespace section
+}
 
 #if defined(_MSC_VER)
-#	pragma warning(pop)
+#   pragma warning(pop)
 #endif
 
-#endif	// end of guard _CEGUIDefaultLogger_h_
+#endif
+
