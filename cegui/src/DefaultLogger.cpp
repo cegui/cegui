a124a602cd733bd7d6853761001ebe22a38e9b7e
Paul Turner <paul@cegui.org.uk>
2012-05-31 20:17:25 +0100

    Reformat / update DefaultLogger files to more closely comply with current coding standards.

--- cegui/src/DefaultLogger.cpp
+++ cegui/src/DefaultLogger.cpp
@@ -6,7 +6,7 @@
     purpose:    Implementation of the DefaultLogger class
 *************************************************************************/
 /***************************************************************************
- *   Copyright (C) 2004 - 2006 Paul D Turner & The CEGUI Development Team
+ *   Copyright (C) 2004 - 2012 Paul D Turner & The CEGUI Development Team
  *
  *   Permission is hereby granted, free of charge, to any person obtaining
  *   a copy of this software and associated documentation files (the
@@ -32,150 +32,149 @@
 #include <ctime>
 #include <iomanip>
 
-// Start of CEGUI namespace section
 namespace CEGUI
 {
+//----------------------------------------------------------------------------//
+DefaultLogger::DefaultLogger(void) :
+    d_caching(true)
+{
+    // create log header
+    logEvent("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
+    logEvent("+                     Crazy Eddie's GUI System - Event log                    +");
+    logEvent("+                          (http://www.cegui.org.uk/)                         +");
+    logEvent("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
+    char addr_buff[32];
+    sprintf(addr_buff, "(%p)", static_cast<void*>(this));
+    logEvent("CEGUI::Logger singleton created. " + String(addr_buff));
+}
 
-    /*************************************************************************
-        Constructor
-    *************************************************************************/
-    DefaultLogger::DefaultLogger(void) :
-            d_caching(true)
+//----------------------------------------------------------------------------//
+DefaultLogger::~DefaultLogger(void)
+{
+    if (d_ostream.is_open())
     {
-        // create log header
-        logEvent("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+");
-        logEvent("+                     Crazy Eddie's GUI System - Event log                    +");
-        logEvent("+                          (http://www.cegui.org.uk/)                         +");
-        logEvent("+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+\n");
         char addr_buff[32];
         sprintf(addr_buff, "(%p)", static_cast<void*>(this));
-        logEvent("CEGUI::Logger singleton created. " + String(addr_buff));
+        logEvent("CEGUI::Logger singleton destroyed. " + String(addr_buff));
+        d_ostream.close();
+    }
+}
+
+//----------------------------------------------------------------------------//
+void DefaultLogger::logEvent(const String& message,
+                             LoggingLevel level /* = Standard */)
+{
+    using namespace std;
+
+    time_t et;
+    time(&et);
+    tm* etm = localtime(&et);
+
+    if (!etm)
+        return;
+
+    // clear sting stream
+    d_workstream.str("");
+
+    // write date
+    d_workstream << setfill('0') << setw(2) << etm->tm_mday << '/' <<
+    setfill('0') << setw(2) << 1 + etm->tm_mon << '/' <<
+    setw(4) << (1900 + etm->tm_year) << ' ';
+
+    // write time
+    d_workstream << setfill('0') << setw(2) << etm->tm_hour << ':' <<
+    setfill('0') << setw(2) << etm->tm_min << ':' <<
+    setfill('0') << setw(2) << etm->tm_sec << ' ';
+
+    // write event type code
+    switch(level)
+    {
+    case Errors:
+        d_workstream << "(Error)\t";
+        break;
+
+    case Warnings:
+        d_workstream << "(Warn)\t";
+        break;
+
+    case Standard:
+        d_workstream << "(Std) \t";
+        break;
+
+    case Informative:
+        d_workstream << "(Info) \t";
+        break;
+
+    case Insane:
+        d_workstream << "(Insan)\t";
+        break;
+
+    default:
+        d_workstream << "(Unkwn)\t";
+        break;
     }
 
-    /*************************************************************************
-        Destructor
-    *************************************************************************/
-    DefaultLogger::~DefaultLogger(void)
+    d_workstream << message << endl;
+
+    if (d_caching)
     {
-        if (d_ostream.is_open())
-        {
-            char addr_buff[32];
-            sprintf(addr_buff, "(%p)", static_cast<void*>(this));
-            logEvent("CEGUI::Logger singleton destroyed. " + String(addr_buff));
-            d_ostream.close();
-        }
+        d_cache.push_back(std::make_pair(d_workstream.str().c_str(), level));
     }
-
-    /*************************************************************************
-        Logs an event
-    *************************************************************************/
-    void DefaultLogger::logEvent(const String& message, LoggingLevel level /* = Standard */)
+    else if (d_level >= level)
     {
-        using namespace std;
+        // write message
+        d_ostream << d_workstream.str();
+        // ensure new event is written to the file, rather than just being
+        // buffered.
+        d_ostream.flush();
+    }
+}
 
-        time_t  et;
-        time(&et);
-        tm* etm = localtime(&et);
+//----------------------------------------------------------------------------//
+void DefaultLogger::setLogFilename(const String& filename, bool append)
+{
+    // close current log file (if any)
+    if (d_ostream.is_open())
+        d_ostream.close();
 
-        if (etm)
+    d_ostream.open(filename.c_str(),
+                   std::ios_base::out |
+                   (append ? std::ios_base::app : std::ios_base::trunc));
+
+    if (!d_ostream)
+        CEGUI_THROW(FileIOException(
+            "[DefaultLogger] Failed to open file '" + filename +
+            "' for writing"));
+
+    // initialise width for date & time alignment.
+    d_ostream.width(2);
+
+    // write out cached log strings.
+    if (d_caching)
+    {
+        d_caching = false;
+
+        Cache::iterator iter = d_cache.begin();
+
+        while (iter != d_cache.end())
         {
-            // clear sting stream
-            d_workstream.str("");
-
-            // write date
-            d_workstream << setfill('0') << setw(2) << etm->tm_mday << '/' <<
-            setfill('0') << setw(2) << 1 + etm->tm_mon << '/' <<
-            setw(4) << (1900 + etm->tm_year) << ' ';
-
-            // write time
-            d_workstream << setfill('0') << setw(2) << etm->tm_hour << ':' <<
-            setfill('0') << setw(2) << etm->tm_min << ':' <<
-            setfill('0') << setw(2) << etm->tm_sec << ' ';
-
-            // write event type code
-            switch(level)
-            {
-            case Errors:
-                d_workstream << "(Error)\t";
-                break;
-
-            case Warnings:
-                d_workstream << "(Warn)\t";
-                break;
-
-            case Standard:
-                d_workstream << "(Std) \t";
-                break;
-
-            case Informative:
-                d_workstream << "(Info) \t";
-                break;
-
-            case Insane:
-                d_workstream << "(Insan)\t";
-                break;
-
-            default:
-                d_workstream << "(Unkwn)\t";
-                break;
-            }
-
-            d_workstream << message << endl;
-
-            if (d_caching)
-            {
-                d_cache.push_back(std::make_pair(d_workstream.str().c_str(), level));
-            }
-            else if (d_level >= level)
+            if (d_level >= (*iter).second)
             {
                 // write message
-                d_ostream << d_workstream.str();
-                // ensure new event is written to the file, rather than just being buffered.
+                d_ostream << (*iter).first;
+                // ensure new event is written to the file, rather than just
+                // being buffered.
                 d_ostream.flush();
             }
+
+            ++iter;
         }
+
+        d_cache.clear();
     }
+}
 
-    void DefaultLogger::setLogFilename(const String& filename, bool append)
-    {
-        // close current log file (if any)
-        if (d_ostream.is_open())
-        {
-            d_ostream.close();
-        }
+//----------------------------------------------------------------------------//
 
-        d_ostream.open(filename.c_str(), std::ios_base::out | (append ? std::ios_base::app : std::ios_base::trunc));
+}
 
-        if (!d_ostream)
-        {
-            CEGUI_THROW(FileIOException("Failed to open file '" + filename + "' for writing"));
-        }
-
-        // initialise width for date & time alignment.
-        d_ostream.width(2);
-
-        // write out cached log strings.
-        if (d_caching)
-        {
-            d_caching = false;
-
-            Cache::iterator iter = d_cache.begin();
-
-            while (iter != d_cache.end())
-            {
-                if (d_level >= (*iter).second)
-                {
-                    // write message
-                    d_ostream << (*iter).first;
-                    // ensure new event is written to the file, rather than just being buffered.
-                    d_ostream.flush();
-                }
-
-                ++iter;
-            }
-
-            d_cache.clear();
-        }
-    }
-
-} // End of  CEGUI namespace section
