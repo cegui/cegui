0134b7f79bb4e397c57b890866619b663694bfd2
Paul Turner <paul@cegui.org.uk>
2012-06-02 17:24:16 +0100

    Regenerated python binding.

--- cegui/src/ScriptModules/Python/bindings/output/CEGUI/EventSet.pypp.cpp
+++ cegui/src/ScriptModules/Python/bindings/output/CEGUI/EventSet.pypp.cpp
@@ -91,14 +91,74 @@ public:
         // I don't understand why this is happening, I think boost::python should use typeid(args).name() and deduce that it's a
         // derived class, not CEGUI::EventArgs base class
         // However this is not happening so I have to go through all EventArgs classes and try casting one after another
-        if (dynamic_cast<const CEGUI::FontEventArgs*>(&args))
+        if (dynamic_cast<const CEGUI::HeaderSequenceEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::HeaderSequenceEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::KeyEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::KeyEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::MouseEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::MouseEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::UpdateEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::UpdateEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::DragDropEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::DragDropEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::TreeEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::TreeEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::ActivationEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::ActivationEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::WindowEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::WindowEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::LinkedEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::LinkedEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::FontEventArgs*>(&args))
 {
     return boost::python::call<bool>(d_callable, static_cast<const CEGUI::FontEventArgs&>(args));
 }
 
-if (dynamic_cast<const CEGUI::DisplayEventArgs*>(&args))
+if (dynamic_cast<const CEGUI::MouseCursorEventArgs*>(&args))
 {
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::DisplayEventArgs&>(args));
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::MouseCursorEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::ElementEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::ElementEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::GUIContextEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::GUIContextEventArgs&>(args));
+}
+
+if (dynamic_cast<const CEGUI::RenderTargetEventArgs*>(&args))
+{
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::RenderTargetEventArgs&>(args));
 }
 
 if (dynamic_cast<const CEGUI::NamedElementEventArgs*>(&args))
@@ -111,59 +171,9 @@ if (dynamic_cast<const CEGUI::ResourceEventArgs*>(&args))
     return boost::python::call<bool>(d_callable, static_cast<const CEGUI::ResourceEventArgs&>(args));
 }
 
-if (dynamic_cast<const CEGUI::RenderTargetEventArgs*>(&args))
+if (dynamic_cast<const CEGUI::DisplayEventArgs*>(&args))
 {
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::RenderTargetEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::GUIContextEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::GUIContextEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::LinkedEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::LinkedEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::HeaderSequenceEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::HeaderSequenceEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::DragDropEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::DragDropEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::KeyEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::KeyEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::UpdateEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::UpdateEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::MouseEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::MouseEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::ActivationEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::ActivationEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::TreeEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::TreeEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::WindowEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::WindowEventArgs&>(args));
+    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::DisplayEventArgs&>(args));
 }
 
 if (dynamic_cast<const CEGUI::RenderQueueEventArgs*>(&args))
@@ -174,16 +184,6 @@ if (dynamic_cast<const CEGUI::RenderQueueEventArgs*>(&args))
 if (dynamic_cast<const CEGUI::AnimationEventArgs*>(&args))
 {
     return boost::python::call<bool>(d_callable, static_cast<const CEGUI::AnimationEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::ElementEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::ElementEventArgs&>(args));
-}
-
-if (dynamic_cast<const CEGUI::MouseCursorEventArgs*>(&args))
-{
-    return boost::python::call<bool>(d_callable, static_cast<const CEGUI::MouseCursorEventArgs&>(args));
 }
 
 if (dynamic_cast<const CEGUI::EventArgs*>(&args))
