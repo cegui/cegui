1c3fd630fc65f91f24b2af1c73c0ed8c4100f733
Paul Turner <paul@cegui.org.uk>
2012-06-03 18:07:31 +0100

    MOD: Implement new clipping in GeomertryBuffer for DirectFB renderer (of course, this renderer is still not working, but I implemented this just the same :-p ).

--- cegui/include/CEGUI/RendererModules/DirectFB/GeometryBuffer.h
+++ cegui/include/CEGUI/RendererModules/DirectFB/GeometryBuffer.h
@@ -65,17 +65,25 @@ public:
     uint getBatchCount() const;
     void setRenderEffect(RenderEffect* effect);
     RenderEffect* getRenderEffect();
+    void setClippingActive(const bool active);
+    bool isClippingActive() const;
 
 protected:
     //! update cached matrix
     void updateMatrix() const;
 
+    //! type to track info for per-texture sub batches of geometry
+    struct BatchInfo
+    {
+        IDirectFBSurface* texture;
+        uint vertexCount;
+        bool clip;
+    };
+
     //! DirectFBRenderer object that created and owns this GeometryBuffer.
     DirectFBRenderer& d_owner;
     //! last texture that was set as active
     DirectFBTexture* d_activeTexture;
-    //! type to track info for per-texture sub batches of geometry
-    typedef std::pair<IDirectFBSurface*, uint> BatchInfo;
     //! type of container that tracks BatchInfos.
     typedef std::vector<BatchInfo> BatchList;
     //! list of texture batches added to the geometry buffer
@@ -86,6 +94,8 @@ protected:
     VertexList d_vertices;
     //! rectangular clip region
     Rectf d_clipRect;
+    //! whether clipping will be active for the current batch
+    bool d_clippingActive;
     //! translation vector
     Vector3f d_translation;
     //! rotation vector
