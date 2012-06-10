627f8dbb8d43a7fbe2a7e07a54c56726799f6161
Paul Turner <paul@cegui.org.uk>
2012-06-10 21:19:17 +0100

    ADD: typedefs for sized integer and unsigned integer types - we now have a full set.

--- cegui/include/CEGUI/Base.h
+++ cegui/include/CEGUI/Base.h
@@ -147,9 +147,15 @@ typedef unsigned short	ushort;
 typedef unsigned int	uint;
 typedef unsigned char	uchar;
 
-typedef unsigned int    uint32;
-typedef unsigned short  uint16;
-typedef unsigned char   uint8;
+typedef long long  int64;
+typedef int        int32;
+typedef short      int16;
+typedef char       int8;
+
+typedef unsigned long long  uint64;
+typedef unsigned int        uint32;
+typedef unsigned short      uint16;
+typedef unsigned char       uint8;
 
 
 /*************************************************************************
