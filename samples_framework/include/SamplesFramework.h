933c6b9a62d41c37917f2c337e0c2378a1713d02
Lukas Meindl <gsoc@lukasmeindl.at>
2012-07-08 16:26:49 +0200

    ADD: Progressbar for sample loading inside the framework

--- samples_framework/include/SamplesFramework.h
+++ samples_framework/include/SamplesFramework.h
@@ -1,30 +1,30 @@
 /***********************************************************************
-    filename:   SampleFramework.h
-    created:    24/5/2012
-    author:     Lukas E Meindl
+filename:   SampleFramework.h
+created:    24/5/2012
+author:     Lukas E Meindl
 *************************************************************************/
 /***************************************************************************
- *   Copyright (C) 2004 - 2012 Paul D Turner & The CEGUI Development Team
- *
- *   Permission is hereby granted, free of charge, to any person obtaining
- *   a copy of this software and associated documentation files (the
- *   "Software"), to deal in the Software without restriction, including
- *   without limitation the rights to use, copy, modify, merge, publish,
- *   distribute, sublicense, and/or sell copies of the Software, and to
- *   permit persons to whom the Software is furnished to do so, subject to
- *   the following conditions:
- *
- *   The above copyright notice and this permission notice shall be
- *   included in all copies or substantial portions of the Software.
- *
- *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- *   IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- *   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- *   OTHER DEALINGS IN THE SOFTWARE.
- ***************************************************************************/
+*   Copyright (C) 2004 - 2012 Paul D Turner & The CEGUI Development Team
+*
+*   Permission is hereby granted, free of charge, to any person obtaining
+*   a copy of this software and associated documentation files (the
+*   "Software"), to deal in the Software without restriction, including
+*   without limitation the rights to use, copy, modify, merge, publish,
+*   distribute, sublicense, and/or sell copies of the Software, and to
+*   permit persons to whom the Software is furnished to do so, subject to
+*   the following conditions:
+*
+*   The above copyright notice and this permission notice shall be
+*   included in all copies or substantial portions of the Software.
+*
+*   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+*   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+*   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+*   IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+*   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+*   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+*   OTHER DEALINGS IN THE SOFTWARE.
+***************************************************************************/
 #ifndef _Samples_Framework_h_
 #define _Samples_Framework_h_
 
@@ -40,6 +40,7 @@ namespace CEGUI
     class String;
     class Texture;
     class PushButton;
+    class ProgressBar;
 }
 
 class SampleData;
@@ -51,7 +52,7 @@ using namespace CEGUI;
 
 /*!
 \brief
-    This is 
+This is 
 */
 class SamplesFramework : public SamplesFrameworkBase
 {
@@ -61,25 +62,22 @@ public:
 
     static void setDefaultResourceGroup(const String& resourceGroup);
 
-    static const String XMLSchemaName;
-    static String s_defaultResourceGroup;
-
     bool initialise();
-
-    void initialiseSamples();
     void cleanupSample();
 
     void addSampleDataCppModule(CEGUI::String sampleName, CEGUI::String summary, CEGUI::String description, SampleType sampleTypeEnum);
-    void addSample(SampleData* sampleData);
-
-    virtual void update(float passedTime);
-    virtual void handleNewWindowSize(float width, float height);
-
-    virtual void drawGUIContexts();
 
     void handleSampleSelection(CEGUI::Window* sampleWindow);
     void handleStartDisplaySample(CEGUI::Window* sampleWindow);
-    void handleStopDisplaySample();
+
+    virtual void update(float passedTime);
+
+    bool updateInitialisationStep();
+
+    void switchToSampleBrowser();
+    virtual void handleNewWindowSize(float width, float height);
+
+    virtual void drawGUIContexts();
 
     SampleData* findSampleData(CEGUI::Window* sampleWindow);
 
@@ -91,27 +89,42 @@ public:
     virtual bool injectMouseWheelChange(float position);
     virtual bool injectMousePosition(float x, float y);
 
-    bool handleExitSampleView(const CEGUI::EventArgs& args);
-    
+    bool handleSampleExitButtonClicked(const CEGUI::EventArgs& args);
+
+    static const String XMLSchemaName;
+    static String s_defaultResourceGroup;
+
 
 protected:
-    void initialiseFrameworkLayout();
+    void initialiseLoadScreenLayout();
 
+    void initialiseSampleBrowserLayout();
     void loadSamplesDataFromXML(const String& filename, const String& resourceGroup);
     void getSampleInstanceFromDLL(SampleData& sampleData);
 
     void unloadSamples();
 
-    CEGUI::Window* d_root;
+    bool initialiseSample(unsigned int sampleNumber);
 
-    CEGUI::PushButton* d_sampleExitButton;
+    void addSample(SampleData* sampleData);
 
-    std::vector<SampleData*> d_samples;
+    void stopDisplaySample();
 
-    MetaDataWindowManager* d_metaDataWinMgr;
-    SamplesBrowserManager* d_samplesWinMgr;
+    CEGUI::Window*              d_root;
 
-    SampleData*     d_selectedSampleData;
+    CEGUI::PushButton*          d_sampleExitButton;
+
+    std::vector<SampleData*>    d_samples;
+
+    MetaDataWindowManager*      d_metaDataWinMgr;
+    SamplesBrowserManager*      d_samplesWinMgr;
+
+    SampleData*                 d_selectedSampleData;
+
+    CEGUI::ProgressBar*         d_loadingProgressBar;
+    CEGUI::Window*              d_loadingScreenText;
+
+    bool                        d_quittingSampleView;
 };
 
 #endif
