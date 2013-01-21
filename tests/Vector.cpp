a8f1876a06d833ba8ed67e9b22e339a959409db2
Hanmac <hanmac@gmx.de>
2013-01-21 20:47:06 +0100

    added more Unittests to Vector Case

--- tests/Vector.cpp
+++ tests/Vector.cpp
@@ -32,13 +32,69 @@
 
 BOOST_AUTO_TEST_SUITE(Vector)
 
-BOOST_AUTO_TEST_CASE(Constants)
+BOOST_AUTO_TEST_CASE(ConstructionAssignment2)
+{
+    CEGUI::Vector2f a(1, 0);
+    CEGUI::Vector2f b(0, 1);
+    CEGUI::Vector2f c(2, 2);
+    CEGUI::Vector2f d(2, 1);
+    CEGUI::Vector2f e(1, 2);
+
+    BOOST_CHECK_EQUAL(a + b, CEGUI::Vector2f(1, 1));
+    BOOST_CHECK_EQUAL(a - b, CEGUI::Vector2f(1, -1));
+    
+    BOOST_CHECK_EQUAL(a * c, CEGUI::Vector2f(2, 0));
+    BOOST_CHECK_EQUAL(b * c, CEGUI::Vector2f(0, 2));
+    BOOST_CHECK_EQUAL(c * d, CEGUI::Vector2f(4, 2));
+    BOOST_CHECK_EQUAL(c * e, CEGUI::Vector2f(2, 4));
+    
+    BOOST_CHECK_EQUAL(a * 2, CEGUI::Vector2f(2, 0));
+    BOOST_CHECK_EQUAL(b * 2, CEGUI::Vector2f(0, 2));
+}
+
+BOOST_AUTO_TEST_CASE(ConstructionAssignment3)
+{
+    CEGUI::Vector3f a(1, 0, 0);
+    CEGUI::Vector3f b(0, 1, 0);
+    CEGUI::Vector3f c(0, 0, 1);
+    
+    BOOST_CHECK_EQUAL(a + b, CEGUI::Vector3f(1, 1, 0));
+    BOOST_CHECK_EQUAL(a - b, CEGUI::Vector3f(1, -1, 0));
+    
+    BOOST_CHECK_EQUAL(a * 2, CEGUI::Vector3f(2, 0, 0));
+    BOOST_CHECK_EQUAL(b * 2, CEGUI::Vector3f(0, 2, 0));
+    BOOST_CHECK_EQUAL(c * 2, CEGUI::Vector3f(0, 0, 2));
+    
+    BOOST_CHECK_EQUAL(CEGUI::Vector3f::one(), CEGUI::Vector3f::one_x() + CEGUI::Vector3f::one_y() + CEGUI::Vector3f::one_z());
+}
+
+BOOST_AUTO_TEST_CASE(Constants2)
 {
     BOOST_CHECK_EQUAL(CEGUI::Vector2f::zero(), CEGUI::Vector2f(0, 0));
     BOOST_CHECK_EQUAL(CEGUI::Vector2f::one(), CEGUI::Vector2f(1, 1));
+    BOOST_CHECK_EQUAL(CEGUI::Vector2f::one_x(), CEGUI::Vector2f(1, 0));
+    BOOST_CHECK_EQUAL(CEGUI::Vector2f::one_y(), CEGUI::Vector2f(0, 1));
     
     BOOST_CHECK_EQUAL(CEGUI::UVector2::zero(), CEGUI::UVector2(CEGUI::UDim(0, 0), CEGUI::UDim(0, 0)));
     BOOST_CHECK_EQUAL(CEGUI::UVector2::one(), CEGUI::UVector2(CEGUI::UDim(1, 0), CEGUI::UDim(1, 0)));
+    BOOST_CHECK_EQUAL(CEGUI::UVector2::one_x(), CEGUI::UVector2(CEGUI::UDim(1, 0), CEGUI::UDim(0, 0)));
+    BOOST_CHECK_EQUAL(CEGUI::UVector2::one_y(), CEGUI::UVector2(CEGUI::UDim(0, 0), CEGUI::UDim(1, 0)));
+    
+    // test if it works with the UDim zero and relative
+    BOOST_CHECK_EQUAL(CEGUI::UVector2::zero(), CEGUI::UVector2(CEGUI::UDim::zero(), CEGUI::UDim::zero()));
+    BOOST_CHECK_EQUAL(CEGUI::UVector2::one(), CEGUI::UVector2(CEGUI::UDim::relative(), CEGUI::UDim::relative()));
+    BOOST_CHECK_EQUAL(CEGUI::UVector2::one_x(), CEGUI::UVector2(CEGUI::UDim::relative(), CEGUI::UDim::zero()));
+    BOOST_CHECK_EQUAL(CEGUI::UVector2::one_y(), CEGUI::UVector2(CEGUI::UDim::zero(), CEGUI::UDim::relative()));
+}
+
+BOOST_AUTO_TEST_CASE(Constants3)
+{
+    BOOST_CHECK_EQUAL(CEGUI::Vector3f::zero(), CEGUI::Vector3f(0, 0, 0));
+    BOOST_CHECK_EQUAL(CEGUI::Vector3f::one(), CEGUI::Vector3f(1, 1, 1));
+    
+    BOOST_CHECK_EQUAL(CEGUI::Vector3f::one_x(), CEGUI::Vector3f(1, 0, 0));
+    BOOST_CHECK_EQUAL(CEGUI::Vector3f::one_y(), CEGUI::Vector3f(0, 1, 0));
+    BOOST_CHECK_EQUAL(CEGUI::Vector3f::one_z(), CEGUI::Vector3f(0, 0, 1));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
