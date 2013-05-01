180d29ccbee9e417a618a51c052104e7e1ee13bf
timotei21 <timotei21@gmail.com>
2013-05-02 00:53:56 +0300

    Fix the tests compilation on Visual Studio

--- tests/Entry.cpp
+++ tests/Entry.cpp
@@ -49,6 +49,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+// If this looks wanky, it's becase it is!  Behold that this is not as fullblown
+// as it could be though.
+// Copied from samples_framework/include/CEGuiBaseApplication.h
+#ifndef PATH_MAX
+#   include <stdlib.h>
+#   ifndef PATH_MAX
+#       include <limits.h>
+#   endif
+#   ifndef PATH_MAX
+#      ifdef _MAX_PATH
+#           define PATH_MAX _MAX_PATH
+#       else
+#           define PATH_MAX 260
+#       endif
+#   endif
+#endif
+
 #ifdef __APPLE__
 #   include <Carbon/Carbon.h>
 #endif
