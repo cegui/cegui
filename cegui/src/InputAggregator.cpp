5fe3ac023c91c01e8743b190416389de18319c75
timotei21 <timotei21@gmail.com>
2013-07-20 13:49:47 +0300

    Unindent 'InputAggregator' methods definitions inside the namespace

--- cegui/src/InputAggregator.cpp
+++ cegui/src/InputAggregator.cpp
@@ -31,229 +31,231 @@
 // Start of CEGUI namespace section
 namespace CEGUI
 {
-    static PointerSource convertToPointerSource(MouseButton button)
+
+static PointerSource convertToPointerSource(MouseButton button)
+{
+    if (button == LeftButton)
+        return PS_Left;
+
+    if (button == RightButton)
+        return PS_Right;
+
+    if (button == MiddleButton)
+        return PS_Middle;
+
+    return PS_None;
+}
+
+InputAggregator::InputAggregator(InputEventReceiver* input_receiver) :
+    d_inputReceiver(input_receiver),
+    d_pointerPosition(0.0f, 0.0f),
+    d_keysPressed()
+{
+    initializeSimpleKeyMappings();
+}
+
+bool InputAggregator::injectTimePulse(float timeElapsed)
+{
+    return true;
+}
+
+bool InputAggregator::injectMouseMove(float delta_x, float delta_y)
+{
+    return injectMousePosition(delta_x + d_pointerPosition.d_x,
+        delta_y + d_pointerPosition.d_y);
+}
+
+bool InputAggregator::injectMousePosition(float x_pos, float y_pos)
+{
+    d_pointerPosition = Vector2f(x_pos, y_pos);
+
+    SemanticInputEvent semantic_event(SV_PointerMove);
+    semantic_event.d_payload.array[0] = x_pos;
+    semantic_event.d_payload.array[1] = y_pos;
+
+    d_inputReceiver->injectInputEvent(&semantic_event);
+
+    return true;
+}
+
+bool InputAggregator::injectMouseLeaves()
+{
+    return true;
+}
+
+bool InputAggregator::injectMouseButtonDown(MouseButton button)
+{
+    SemanticValue value = SV_PointerHold;
+    if (isControlPressed())
+        value = SV_SelectCumulative;
+    else if (isShiftPressed())
+        value = SV_SelectMultipleItems;
+
+    SemanticInputEvent semantic_event(value);
+    semantic_event.d_payload.source = convertToPointerSource(button);
+
+    d_inputReceiver->injectInputEvent(&semantic_event);
+
+    return true;
+}
+bool InputAggregator::injectMouseButtonUp(MouseButton button)
+{
+    SemanticInputEvent semantic_event(SV_PointerActivate);
+    semantic_event.d_payload.source = convertToPointerSource(button);
+
+    d_inputReceiver->injectInputEvent(&semantic_event);
+
+    return true;
+}
+
+bool InputAggregator::injectKeyDown(Key::Scan scan_code)
+{
+    d_keysPressed[scan_code] = true;
+    return true;
+}
+
+bool InputAggregator::injectKeyUp(Key::Scan scan_code)
+{
+    SemanticValue value = d_keyValuesMappings[scan_code];
+
+    // handle combined keys
+    if (isControlPressed())
     {
-        if (button == LeftButton)
-            return PS_Left;
-
-        if (button == RightButton)
-            return PS_Right;
-
-        if (button == MiddleButton)
-            return PS_Middle;
-
-        return PS_None;
+        if (scan_code == Key::ArrowLeft)
+            value = SV_GoToPreviousWord;
+        else if (scan_code == Key::ArrowRight)
+            value = SV_GoToNextWord;
+    }
+    else if (isShiftPressed())
+    {
+        if (scan_code == Key::ArrowLeft)
+            value = SV_SelectPreviousCharacter;
+        else if (scan_code == Key::ArrowRight)
+            value = SV_SelectNextCharacter;
     }
 
-    InputAggregator::InputAggregator(InputEventReceiver* input_receiver) :
-        d_inputReceiver(input_receiver),
-        d_pointerPosition(0.0f, 0.0f),
-        d_keysPressed()
+    if (value != SV_NoValue)
     {
-        initializeSimpleKeyMappings();
-    }
-
-    bool InputAggregator::injectTimePulse(float timeElapsed)
-    {
-        return true;
-    }
-
-    bool InputAggregator::injectMouseMove(float delta_x, float delta_y)
-    {
-        return injectMousePosition(delta_x + d_pointerPosition.d_x,
-            delta_y + d_pointerPosition.d_y);
-    }
-
-    bool InputAggregator::injectMousePosition(float x_pos, float y_pos)
-    {
-        d_pointerPosition = Vector2f(x_pos, y_pos);
-
-        SemanticInputEvent semantic_event(SV_PointerMove);
-        semantic_event.d_payload.array[0] = x_pos;
-        semantic_event.d_payload.array[1] = y_pos;
-
-        d_inputReceiver->injectInputEvent(&semantic_event);
-
-        return true;
-    }
-
-    bool InputAggregator::injectMouseLeaves()
-    {
-        return true;
-    }
-
-    bool InputAggregator::injectMouseButtonDown(MouseButton button)
-    {
-        SemanticValue value = SV_PointerHold;
-        if (isControlPressed())
-            value = SV_SelectCumulative;
-        else if (isShiftPressed())
-            value = SV_SelectMultipleItems;
-
         SemanticInputEvent semantic_event(value);
-        semantic_event.d_payload.source = convertToPointerSource(button);
 
         d_inputReceiver->injectInputEvent(&semantic_event);
 
-        return true;
+        d_keysPressed[scan_code] = false;
     }
-    bool InputAggregator::injectMouseButtonUp(MouseButton button)
-    {
-        SemanticInputEvent semantic_event(SV_PointerActivate);
-        semantic_event.d_payload.source = convertToPointerSource(button);
+    return true;
+}
 
-        d_inputReceiver->injectInputEvent(&semantic_event);
+bool InputAggregator::injectChar(String::value_type code_point)
+{
+    TextInputEvent text_event;
+    text_event.d_character = code_point;
 
-        return true;
-    }
+    d_inputReceiver->injectInputEvent(&text_event);
+    return true;
+}
 
-    bool InputAggregator::injectKeyDown(Key::Scan scan_code)
-    {
-        d_keysPressed[scan_code] = true;
-        return true;
-    }
+bool InputAggregator::injectMouseWheelChange(float delta)
+{
+    SemanticInputEvent semantic_event(SV_VerticalScroll);
+    semantic_event.d_payload.single = delta;
 
-    bool InputAggregator::injectKeyUp(Key::Scan scan_code)
-    {
-        SemanticValue value = d_keyValuesMappings[scan_code];
+    d_inputReceiver->injectInputEvent(&semantic_event);
 
-        // handle combined keys
-        if (isControlPressed())
-        {
-            if (scan_code == Key::ArrowLeft)
-                value = SV_GoToPreviousWord;
-            else if (scan_code == Key::ArrowRight)
-                value = SV_GoToNextWord;
-        }
-        else if (isShiftPressed())
-        {
-            if (scan_code == Key::ArrowLeft)
-                value = SV_SelectPreviousCharacter;
-            else if (scan_code == Key::ArrowRight)
-                value = SV_SelectNextCharacter;
-        }
+    return true;
+}
 
-        if (value != SV_NoValue)
-        {
-            SemanticInputEvent semantic_event(value);
+bool InputAggregator::injectMouseButtonClick(const MouseButton button)
+{
+    SemanticInputEvent semantic_event(SV_PointerActivate);
 
-            d_inputReceiver->injectInputEvent(&semantic_event);
+    if (isControlPressed())
+        semantic_event.d_value = SV_SelectCumulative;
 
-            d_keysPressed[scan_code] = false;
-        }
-        return true;
-    }
+    semantic_event.d_payload.source = convertToPointerSource(button);
 
-    bool InputAggregator::injectChar(String::value_type code_point)
-    {
-        TextInputEvent text_event;
-        text_event.d_character = code_point;
+    d_inputReceiver->injectInputEvent(&semantic_event);
 
-        d_inputReceiver->injectInputEvent(&text_event);
-        return true;
-    }
+    return true;
+}
 
-    bool InputAggregator::injectMouseWheelChange(float delta)
-    {
-        SemanticInputEvent semantic_event(SV_VerticalScroll);
-        semantic_event.d_payload.single = delta;
+bool InputAggregator::injectMouseButtonDoubleClick(const MouseButton button)
+{
+    SemanticInputEvent semantic_event(SV_SelectWord);
+    semantic_event.d_payload.source = convertToPointerSource(button);
 
-        d_inputReceiver->injectInputEvent(&semantic_event);
+    d_inputReceiver->injectInputEvent(&semantic_event);
 
-        return true;
-    }
+    return true;
+}
 
-    bool InputAggregator::injectMouseButtonClick(const MouseButton button)
-    {
-        SemanticInputEvent semantic_event(SV_PointerActivate);
+bool InputAggregator::injectMouseButtonTripleClick(const MouseButton button)
+{
+    SemanticInputEvent semantic_event(SV_SelectAll);
+    semantic_event.d_payload.source = convertToPointerSource(button);
 
-        if (isControlPressed())
-            semantic_event.d_value = SV_SelectCumulative;
+    d_inputReceiver->injectInputEvent(&semantic_event);
 
-        semantic_event.d_payload.source = convertToPointerSource(button);
+    return true;
+}
 
-        d_inputReceiver->injectInputEvent(&semantic_event);
+bool InputAggregator::injectCopyRequest()
+{
+    SemanticInputEvent semantic_event(SV_Copy);
 
-        return true;
-    }
+    d_inputReceiver->injectInputEvent(&semantic_event);
 
-    bool InputAggregator::injectMouseButtonDoubleClick(const MouseButton button)
-    {
-        SemanticInputEvent semantic_event(SV_SelectWord);
-        semantic_event.d_payload.source = convertToPointerSource(button);
+    return true;
+}
 
-        d_inputReceiver->injectInputEvent(&semantic_event);
+bool InputAggregator::injectCutRequest()
+{
+    SemanticInputEvent semantic_event(SV_Cut);
 
-        return true;
-    }
+    d_inputReceiver->injectInputEvent(&semantic_event);
 
-    bool InputAggregator::injectMouseButtonTripleClick(const MouseButton button)
-    {
-        SemanticInputEvent semantic_event(SV_SelectAll);
-        semantic_event.d_payload.source = convertToPointerSource(button);
+    return true;
+}
 
-        d_inputReceiver->injectInputEvent(&semantic_event);
+bool InputAggregator::injectPasteRequest()
+{
+    SemanticInputEvent semantic_event(SV_Paste);
 
-        return true;
-    }
+    d_inputReceiver->injectInputEvent(&semantic_event);
 
-    bool InputAggregator::injectCopyRequest()
-    {
-        SemanticInputEvent semantic_event(SV_Copy);
+    return true;
+}
 
-        d_inputReceiver->injectInputEvent(&semantic_event);
+void InputAggregator::initializeSimpleKeyMappings()
+{
+    // Initialize the array
+    memset(d_keyValuesMappings, SV_NoValue, sizeof(SemanticValue) * 0xFF);
 
-        return true;
-    }
+    d_keyValuesMappings[Key::Backspace] = SV_DeletePreviousCharacter;
+    d_keyValuesMappings[Key::Delete] = SV_DeleteNextCharacter;
 
-    bool InputAggregator::injectCutRequest()
-    {
-        SemanticInputEvent semantic_event(SV_Cut);
+    d_keyValuesMappings[Key::NumpadEnter] = SV_Confirm;
+    d_keyValuesMappings[Key::Tab] = SV_Confirm;
+    d_keyValuesMappings[Key::Return] = SV_Confirm;
 
-        d_inputReceiver->injectInputEvent(&semantic_event);
+    d_keyValuesMappings[Key::ArrowLeft] = SV_GoToPreviousCharacter;
+    d_keyValuesMappings[Key::ArrowRight] = SV_GoToNextCharacter;
+    d_keyValuesMappings[Key::End] = SV_GoToEndOfLine;
+    d_keyValuesMappings[Key::Home] = SV_GoToStartOfLine;
+}
 
-        return true;
-    }
+bool InputAggregator::isShiftPressed()
+{
+    return d_keysPressed[Key::LeftShift] || d_keysPressed[Key::RightShift];
+}
 
-    bool InputAggregator::injectPasteRequest()
-    {
-        SemanticInputEvent semantic_event(SV_Paste);
+bool InputAggregator::isAltPressed()
+{
+    return d_keysPressed[Key::LeftAlt] || d_keysPressed[Key::RightAlt];
+}
 
-        d_inputReceiver->injectInputEvent(&semantic_event);
+bool InputAggregator::isControlPressed()
+{
+    return d_keysPressed[Key::LeftControl] || d_keysPressed[Key::RightControl];
+}
 
-        return true;
-    }
-
-    void InputAggregator::initializeSimpleKeyMappings()
-    {
-        // Initialize the array
-        memset(d_keyValuesMappings, SV_NoValue, sizeof(SemanticValue) * 0xFF);
-
-        d_keyValuesMappings[Key::Backspace] = SV_DeletePreviousCharacter;
-        d_keyValuesMappings[Key::Delete] = SV_DeleteNextCharacter;
-
-        d_keyValuesMappings[Key::NumpadEnter] = SV_Confirm;
-        d_keyValuesMappings[Key::Tab] = SV_Confirm;
-        d_keyValuesMappings[Key::Return] = SV_Confirm;
-
-        d_keyValuesMappings[Key::ArrowLeft] = SV_GoToPreviousCharacter;
-        d_keyValuesMappings[Key::ArrowRight] = SV_GoToNextCharacter;
-        d_keyValuesMappings[Key::End] = SV_GoToEndOfLine;
-        d_keyValuesMappings[Key::Home] = SV_GoToStartOfLine;
-    }
-
-    bool InputAggregator::isShiftPressed()
-    {
-        return d_keysPressed[Key::LeftShift] || d_keysPressed[Key::RightShift];
-    }
-
-    bool InputAggregator::isAltPressed()
-    {
-        return d_keysPressed[Key::LeftAlt] || d_keysPressed[Key::RightAlt];
-    }
-
-    bool InputAggregator::isControlPressed()
-    {
-        return d_keysPressed[Key::LeftControl] || d_keysPressed[Key::RightControl];
-    }
 } // End of  CEGUI namespace section
