9458e27e20ff027c9eeb76149ff609993723f4bb
timotei21 <timotei21@gmail.com>
2013-07-20 13:48:27 +0300

    Implement the key-related semantic input events

--- cegui/src/InputAggregator.cpp
+++ cegui/src/InputAggregator.cpp
@@ -47,8 +47,10 @@ namespace CEGUI
 
     InputAggregator::InputAggregator(InputEventReceiver* input_receiver) :
         d_inputReceiver(input_receiver),
-        d_pointerPosition(0.0f, 0.0f)
+        d_pointerPosition(0.0f, 0.0f),
+        d_keysPressed()
     {
+        initializeSimpleKeyMappings();
     }
 
     bool InputAggregator::injectTimePulse(float timeElapsed)
@@ -82,7 +84,13 @@ namespace CEGUI
 
     bool InputAggregator::injectMouseButtonDown(MouseButton button)
     {
-        SemanticInputEvent semantic_event(SV_PointerHold);
+        SemanticValue value = SV_PointerHold;
+        if (isControlPressed())
+            value = SV_SelectCumulative;
+        else if (isShiftPressed())
+            value = SV_SelectMultipleItems;
+
+        SemanticInputEvent semantic_event(value);
         semantic_event.d_payload.source = convertToPointerSource(button);
 
         d_inputReceiver->injectInputEvent(&semantic_event);
@@ -101,13 +109,38 @@ namespace CEGUI
 
     bool InputAggregator::injectKeyDown(Key::Scan scan_code)
     {
-        /* Ignored for now */
+        d_keysPressed[scan_code] = true;
         return true;
     }
 
     bool InputAggregator::injectKeyUp(Key::Scan scan_code)
     {
+        SemanticValue value = d_keyValuesMappings[scan_code];
 
+        // handle combined keys
+        if (isControlPressed())
+        {
+            if (scan_code == Key::ArrowLeft)
+                value = SV_GoToPreviousWord;
+            else if (scan_code == Key::ArrowRight)
+                value = SV_GoToNextWord;
+        }
+        else if (isShiftPressed())
+        {
+            if (scan_code == Key::ArrowLeft)
+                value = SV_SelectPreviousCharacter;
+            else if (scan_code == Key::ArrowRight)
+                value = SV_SelectNextCharacter;
+        }
+
+        if (value != SV_NoValue)
+        {
+            SemanticInputEvent semantic_event(value);
+
+            d_inputReceiver->injectInputEvent(&semantic_event);
+
+            d_keysPressed[scan_code] = false;
+        }
         return true;
     }
 
@@ -133,6 +166,10 @@ namespace CEGUI
     bool InputAggregator::injectMouseButtonClick(const MouseButton button)
     {
         SemanticInputEvent semantic_event(SV_PointerActivate);
+
+        if (isControlPressed())
+            semantic_event.d_value = SV_SelectCumulative;
+
         semantic_event.d_payload.source = convertToPointerSource(button);
 
         d_inputReceiver->injectInputEvent(&semantic_event);
@@ -185,5 +222,38 @@ namespace CEGUI
         d_inputReceiver->injectInputEvent(&semantic_event);
 
         return true;
+    }
+
+    void InputAggregator::initializeSimpleKeyMappings()
+    {
+        // Initialize the array
+        memset(d_keyValuesMappings, SV_NoValue, sizeof(SemanticValue) * 0xFF);
+
+        d_keyValuesMappings[Key::Backspace] = SV_DeletePreviousCharacter;
+        d_keyValuesMappings[Key::Delete] = SV_DeleteNextCharacter;
+
+        d_keyValuesMappings[Key::NumpadEnter] = SV_Confirm;
+        d_keyValuesMappings[Key::Tab] = SV_Confirm;
+        d_keyValuesMappings[Key::Return] = SV_Confirm;
+
+        d_keyValuesMappings[Key::ArrowLeft] = SV_GoToPreviousCharacter;
+        d_keyValuesMappings[Key::ArrowRight] = SV_GoToNextCharacter;
+        d_keyValuesMappings[Key::End] = SV_GoToEndOfLine;
+        d_keyValuesMappings[Key::Home] = SV_GoToStartOfLine;
+    }
+
+    bool InputAggregator::isShiftPressed()
+    {
+        return d_keysPressed[Key::LeftShift] || d_keysPressed[Key::RightShift];
+    }
+
+    bool InputAggregator::isAltPressed()
+    {
+        return d_keysPressed[Key::LeftAlt] || d_keysPressed[Key::RightAlt];
+    }
+
+    bool InputAggregator::isControlPressed()
+    {
+        return d_keysPressed[Key::LeftControl] || d_keysPressed[Key::RightControl];
     }
 } // End of  CEGUI namespace section
