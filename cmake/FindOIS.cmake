8f5331b0f3f20f0c17aa45148989539dbd35e5c5
Augustin Preda <Augustin Preda@localhost>
2013-10-18 21:22:04 +0300

    Improved detection of OIS, with default paths built by the Ogre dependencies package.

--- cmake/FindOIS.cmake
+++ cmake/FindOIS.cmake
@@ -3,10 +3,28 @@
 ################################################################################
 include(FindPackageHandleStandardArgs)
 
-find_path(OIS_H_PATH NAMES OIS.h PATH_SUFFIXES OIS)
-find_library(OIS_LIB NAMES OIS libOIS)
-find_library(OIS_LIB_DBG NAMES OIS_d libOIS_d)
+set(ENV_OIS_HOME $ENV{OIS_HOME})
+if (ENV_OIS_HOME)
+  string( REGEX REPLACE "\\\\" "/" ENV_OIS_HOME ${ENV_OIS_HOME} )
+endif ()
 
+set(OIS_PREFIX_PATH
+  ${OIS_HOME} ${ENV_OIS_HOME}
+)
+
+
+find_path(OIS_H_PATH NAMES OIS.h
+        PATHS ${OIS_PREFIX_PATH} 
+        PATH_SUFFIXES OIS include includes)
+        
+find_library(OIS_LIB NAMES OIS libOIS
+        PATHS ${OIS_PREFIX_PATH}
+        PATH_SUFFIXES Release lib/Release
+        )
+find_library(OIS_LIB_DBG NAMES OIS_d libOIS_d
+        PATHS ${OIS_PREFIX_PATH}
+        PATH_SUFFIXES Debug lib/Debug
+        )
 mark_as_advanced(OIS_H_PATH OIS_LIB OIS_LIB_DBG)
 
 find_package_handle_standard_args(OIS DEFAULT_MSG OIS_LIB OIS_H_PATH)
