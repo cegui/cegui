14f8667367cd0b576534113144335edbb98688f5
Timotei Dolean <timotei21@gmail.com>
2014-05-25 00:17:54 +0300

    Add the initial ListView files and ItemView base class
    This will be used as base class for all future views.
    Also add the "views" subdirectory into the cmake script

---
+++ cegui/include/CEGUI/views/ListView.h
@@ -0,0 +1,39 @@
+/***********************************************************************
+    filename:   ListView.h
+    created:    Sat May 24 2014
+    author:     Timotei Dolean <timotei21@gmail.com>
+
+    purpose:    Interface for a view that displays a list of model items.
+*************************************************************************/
+/***************************************************************************
+ *   Copyright (C) 2004 - 2014 Paul D Turner & The CEGUI Development Team
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining
+ *   a copy of this software and associated documentation files (the
+ *   "Software"), to deal in the Software without restriction, including
+ *   without limitation the rights to use, copy, modify, merge, publish,
+ *   distribute, sublicense, and/or sell copies of the Software, and to
+ *   permit persons to whom the Software is furnished to do so, subject to
+ *   the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be
+ *   included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *   IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *   OTHER DEALINGS IN THE SOFTWARE.
+ ***************************************************************************/
+#ifndef _CEGUIListView_h_
+#define _CEGUIListView_h_
+
+// Start of CEGUI namespace section
+namespace CEGUI
+{
+
+} // End of  CEGUI namespace section
+
+#endif  // end of guard _CEGUIListView_h_
