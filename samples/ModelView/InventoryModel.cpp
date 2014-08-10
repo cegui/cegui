d363ba8419f110187efdc2e1e360f2c89767bddf
Timotei Dolean <timotei21@gmail.com>
2014-08-11 00:19:30 +0300

    Make InventoryModel inherit from AbstractItemModel

--- samples/ModelView/InventoryModel.cpp
+++ samples/ModelView/InventoryModel.cpp
@@ -31,28 +31,16 @@
 
 using namespace CEGUI;
 
-template<typename T>
-static ModelIndex makeValidIndex(size_t id, std::vector<T>& vector)
-{
-    if (id >= 0 && id < vector.size())
-        return ModelIndex(vector.at(id));
-
-    return ModelIndex();
-}
-
 //----------------------------------------------------------------------------//
-InventoryModel::InventoryModel() :
-    d_inventoryRoot(InventoryItem::make("Inventory", 0.0f)),
-    d_randomItemsCount(0)
+InventoryItem::InventoryItem(String name) : AbstractItem(name)
 {
 }
 
 //----------------------------------------------------------------------------//
-InventoryItem* InventoryItem::make(const String& name, float weight, InventoryItem* parent)
+InventoryItem* InventoryItem::make(const String& name, float weight, AbstractItem* parent)
 {
-    InventoryItem* item = new InventoryItem;
+    InventoryItem* item = new InventoryItem(name);
 
-    item->d_name = name;
     item->d_weight = weight;
     item->d_parent = parent;
 
@@ -62,32 +50,23 @@ InventoryItem* InventoryItem::make(const String& name, float weight, InventoryIt
 //----------------------------------------------------------------------------//
 bool InventoryItem::operator==(const InventoryItem& other)
 {
-    if (d_weight != other.d_weight) return false;
-    if (d_name != other.d_name) return false;
-    if (d_items.size() != other.d_items.size()) return false;
+    if (!AbstractItem::operator==(other))
+        return false;
 
-    for (size_t i = 0; i < d_items.size(); ++i)
-    {
-        if (d_items.at(i) != other.d_items.at(i))
-            return false;
-    }
-    return true;
+    return d_weight == other.d_weight;
 }
 
 //----------------------------------------------------------------------------//
-bool InventoryItem::operator!=(const InventoryItem& other)
+InventoryModel::InventoryModel() :
+AbstractItemModel<InventoryItem>(InventoryItem::make("Inventory", 0.0f)),
+d_randomItemsCount(0)
 {
-    return !(*this == other);
 }
 
-//----------------------------------------------------------------------------//
-InventoryItem::InventoryItem() : d_parent(0)
-{
-}
 //----------------------------------------------------------------------------//
 void InventoryModel::load()
 {
-    InventoryItem* backpack = InventoryItem::make("Trip backpack", 2.0f, d_inventoryRoot);
+    InventoryItem* backpack = InventoryItem::make("Trip backpack", 2.0f, d_root);
     InventoryItem* prev_matryoshka = 0;
 
     // matryoshka Z to A
@@ -95,122 +74,52 @@ void InventoryModel::load()
     {
         InventoryItem* matryoshka = InventoryItem::make("Matryoshka " + String(1, chr), 1.0f, backpack);
 
-        matryoshka->d_icon = "DriveIcons/DriveStack";
+        matryoshka->setIcon("DriveIcons/DriveStack");
 
         if (prev_matryoshka != 0)
         {
-            prev_matryoshka->d_parent = matryoshka;
-            matryoshka->d_items.push_back(prev_matryoshka);
+            prev_matryoshka->setParent(matryoshka);
+            matryoshka->addItem(prev_matryoshka);
         }
 
         prev_matryoshka = matryoshka;
     }
 
-    backpack->d_items.push_back(InventoryItem::make("Gepäckaufbewahrungsschein: Wiener Neustadt", 1.0f, backpack));
-    backpack->d_items.push_back(InventoryItem::make("Gepäckaufbewahrungsschein: Frankfurt am Main", 1.0f, backpack));
-    backpack->d_items.push_back(InventoryItem::make("Gepäckaufbewahrungsschein: Sankt Johann im Pongau", 1.0f, backpack));
-    backpack->d_items.push_back(InventoryItem::make("Gepäckaufbewahrungsschein: Seekirchen am Wallersee", 1.0f, backpack));
+    backpack->addItem(InventoryItem::make("Gepäckaufbewahrungsschein: Wiener Neustadt", 1.0f, backpack));
+    backpack->addItem(InventoryItem::make("Gepäckaufbewahrungsschein: Frankfurt am Main", 1.0f, backpack));
+    backpack->addItem(InventoryItem::make("Gepäckaufbewahrungsschein: Sankt Johann im Pongau", 1.0f, backpack));
+    backpack->addItem(InventoryItem::make("Gepäckaufbewahrungsschein: Seekirchen am Wallersee", 1.0f, backpack));
 
     InventoryItem* beans_can = InventoryItem::make("Beans can", 1.0f, backpack);
-    beans_can->d_icon = "DriveIcons/GlobalDrive";
+    beans_can->setIcon("DriveIcons/GlobalDrive");
     InventoryItem* beans = InventoryItem::make("Beans!", 0.1f, beans_can);
-    beans_can->d_items.push_back(beans);
+    beans_can->addItem(beans);
 
-    backpack->d_items.push_back(prev_matryoshka);
-    backpack->d_items.push_back(beans_can);
+    backpack->addItem(prev_matryoshka);
+    backpack->addItem(beans_can);
 
-    d_inventoryRoot->d_items.push_back(backpack);
+    d_root->addItem(backpack);
 
-    InventoryItem* bow = InventoryItem::make("Bow", 23.451f, d_inventoryRoot);
+    InventoryItem* bow = InventoryItem::make("Bow", 23.451f, d_root);
     for (int i = 25; i >= 0; --i)
     {
         InventoryItem* arrow = InventoryItem::make(
             "arrow " + PropertyHelper<int>::toString(i), 0.2f, bow);
-        bow->d_items.push_back(arrow);
+        bow->addItem(arrow);
     }
-    d_inventoryRoot->d_items.push_back(bow);
+    d_root->addItem(bow);
 
     // generate *many* items :D
     for (int i = 2000; i >= 1960; i -= 2)
     {
         InventoryItem* almanach = InventoryItem::make(
-            "Almanach " + PropertyHelper<int>::toString(i), 0.34f, d_inventoryRoot);
+            "Almanach " + PropertyHelper<int>::toString(i), 0.34f, d_root);
 
         if (i % 2 == 0)
-            almanach->d_icon = "DriveIcons/Lime";
+            almanach->setIcon("DriveIcons/Lime");
 
-        d_inventoryRoot->d_items.push_back(almanach);
+        d_root->addItem(almanach);
     }
-}
-
-//----------------------------------------------------------------------------//
-bool InventoryModel::isValidIndex(const ModelIndex& model_index) const
-{
-    //TODO: check if we still have a reference to model_index.userdata?
-    return model_index.d_modelData != 0;
-}
-
-//----------------------------------------------------------------------------//
-ModelIndex InventoryModel::makeIndex(size_t child, const ModelIndex& parent_index)
-{
-    if (parent_index.d_modelData == 0)
-        return ModelIndex();
-
-    InventoryItem* item = static_cast<InventoryItem*>(parent_index.d_modelData);
-    return makeValidIndex(child, item->d_items);
-}
-
-//----------------------------------------------------------------------------//
-ModelIndex InventoryModel::getParentIndex(const ModelIndex& model_index) const
-{
-    if (model_index.d_modelData == d_inventoryRoot)
-        return ModelIndex();
-
-    InventoryItem* item = static_cast<InventoryItem*>(model_index.d_modelData);
-    if (item->d_parent == 0)
-        return getRootIndex();
-
-    return ModelIndex(item->d_parent);
-}
-
-//----------------------------------------------------------------------------//
-ModelIndex InventoryModel::getRootIndex() const
-{
-    return ModelIndex(d_inventoryRoot);
-}
-
-//----------------------------------------------------------------------------//
-size_t InventoryModel::getChildCount(const ModelIndex& model_index) const
-{
-    if (model_index.d_modelData == 0)
-        return d_inventoryRoot->d_items.size();
-
-    return static_cast<InventoryItem*>(model_index.d_modelData)->d_items.size();
-}
-
-//----------------------------------------------------------------------------//
-String InventoryModel::getData(const ModelIndex& model_index, ItemDataRole role /*= IDR_Text*/)
-{
-    if (model_index.d_modelData == 0)
-        return "";
-
-    InventoryItem* item = static_cast<InventoryItem*>(model_index.d_modelData);
-    if (role == IDR_Text)
-        return item->d_name;
-
-    if (role == IDR_Tooltip)
-        return "Tooltip for: " + item->d_name;
-
-    if (role == IDR_Icon)
-        return item->d_icon;
-
-    return "";
-}
-
-//----------------------------------------------------------------------------//
-void InventoryModel::clear(bool notify)
-{
-    deleteChildren(d_inventoryRoot, notify);
 }
 
 //----------------------------------------------------------------------------//
@@ -227,115 +136,18 @@ void InventoryModel::addRandomItemWithChildren(const ModelIndex& parent, size_t 
             "New sub item #" + PropertyHelper<int>::toString(d_randomItemsCount),
             1.3f, new_item);
 
-        new_item->d_items.push_back(new_subitem);
+        new_item->addItem(new_subitem);
     }
 
-    addItem(parent, new_item, position);
+    addItemAtPosition(new_item, parent, position);
     d_randomItemsCount++;
-}
-
-//----------------------------------------------------------------------------//
-void InventoryModel::addItem(const ModelIndex& parent, InventoryItem* new_item, size_t position)
-{
-    InventoryItem* item = static_cast<InventoryItem*>(parent.d_modelData);
-    item->d_items.insert(item->d_items.begin() + position, new_item);
-
-    notifyChildrenAdded(parent, position, 1);
-}
-
-//----------------------------------------------------------------------------//
-bool InventoryModel::areIndicesEqual(const ModelIndex& index1, const ModelIndex& index2) const
-{
-    return index1.d_modelData == index2.d_modelData;
-}
-
-//----------------------------------------------------------------------------//
-int InventoryModel::compareIndices(const ModelIndex& index1, const ModelIndex& index2) const
-{
-    if (areIndicesEqual(index1, index2))
-        return 0;
-
-    InventoryItem* item1 = static_cast<InventoryItem*>(index1.d_modelData);
-    InventoryItem* item2 = static_cast<InventoryItem*>(index2.d_modelData);
-
-    return item1->d_name.compare(item2->d_name);
-}
-
-//----------------------------------------------------------------------------//
-int InventoryModel::getChildId(const ModelIndex& model_index) const
-{
-    ModelIndex parent_index = getParentIndex(model_index);
-    if (!isValidIndex(parent_index))
-        return -1;
-
-    InventoryItem* parent_item = static_cast<InventoryItem*>(parent_index.d_modelData);
-    InventoryItem* child_item = static_cast<InventoryItem*>(model_index.d_modelData);
-
-    std::vector<InventoryItem*>::iterator itor = std::find(
-        parent_item->d_items.begin(), parent_item->d_items.end(), child_item);
-
-    if (itor == parent_item->d_items.end())
-        return -1;
-
-    return std::distance(parent_item->d_items.begin(), itor);
-}
-
-//----------------------------------------------------------------------------//
-void InventoryModel::removeItem(const ModelIndex& index)
-{
-    ModelIndex parent_index = getParentIndex(index);
-    InventoryItem* parent_item = static_cast<InventoryItem*>(parent_index.d_modelData);
-    InventoryItem* child_item = static_cast<InventoryItem*>(index.d_modelData);
-
-    std::vector<InventoryItem*>::iterator itor = std::find(
-        parent_item->d_items.begin(), parent_item->d_items.end(), child_item);
-
-    if (itor != parent_item->d_items.end())
-    {
-        size_t child_id = std::distance(parent_item->d_items.begin(), itor);
-
-        deleteChildren(*itor, true);
-        delete *itor;
-        parent_item->d_items.erase(itor);
-
-        notifyChildrenRemoved(parent_index, child_id, 1);
-    }
-}
-
-//----------------------------------------------------------------------------//
-InventoryModel::~InventoryModel()
-{
-    clear(false);
-    delete d_inventoryRoot;
-}
-
-//----------------------------------------------------------------------------//
-void InventoryModel::deleteChildren(InventoryItem* item, bool notify)
-{
-    if (item == 0)
-        return;
-
-    size_t items_count = item->d_items.size();
-    std::vector<InventoryItem*>::iterator itor = item->d_items.begin();
-
-    while (itor != item->d_items.end())
-    {
-        deleteChildren(*itor, notify);
-        delete *itor;
-        itor = item->d_items.erase(itor);
-    }
-
-    if (notify)
-    {
-        notifyChildrenRemoved(ModelIndex(item), 0, items_count);
-    }
 }
 
 //----------------------------------------------------------------------------//
 void InventoryModel::updateItemName(const ModelIndex& index, const String& newName)
 {
     InventoryItem* item = static_cast<InventoryItem*>(index.d_modelData);
-    item->d_name = newName;
+    item->setText(newName);
 
     notifyChildrenDataChanged(getParentIndex(index), 0, 1);
 }
@@ -343,5 +155,5 @@ void InventoryModel::updateItemName(const ModelIndex& index, const String& newNa
 //----------------------------------------------------------------------------//
 std::ostream& operator<<(std::ostream& output, const InventoryItem& item)
 {
-    return output << item.d_name << " [" << item.d_weight << "]";
+    return output << item.getText() << " [" << item.d_weight << "]";
 }
