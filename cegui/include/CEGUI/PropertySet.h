88479de00751036e27b88607b493687967296b68
Martin Preisler <martin@preisler.me>
2014-08-10 17:44:21 +0200

    DOC: PropertySet documentation improvements, minor code style fixes

--- cegui/include/CEGUI/PropertySet.h
+++ cegui/include/CEGUI/PropertySet.h
@@ -52,54 +52,103 @@ namespace CEGUI
 /*!
 \brief
 	Class that contains a collection of Property objects.
+
+CEGUI uses this interface for introspection and serialisation, especially in
+CEGUI::Window and classes that inherit it.
+
+If you are just a user of an existing PropertySet class there are just 2 methods
+that you should be aware of - PropertySet::setProperty and PropertySet::getProperty.
+Both methods are available in 2 variants - string fallback mode and templated
+native mode.  It is recommended to use the native mode. Code example:
+
+\code{.cpp}
+CEGUI::Window* wnd = ...;
+// native mode property set
+wnd->setProperty<float>("Alpha", 0.5f); // set Alpha to 50%
+// string fallback mode
+wnd->setProperty("Alpha", "0.5"); // set Alpha to 50%
+
+// native mode property get
+const float alpha = wnd->getProperty<float>("Alpha");
+// string fallback mode
+const String alphaString = wnd->getProperty("Alpha");
+\endcode
+
+If you use native mode with the wrong type CEGUI will resort to string fallback
+and will try to convert the type to whatever you requested.
+
+\code{.cpp}
+CEGUI::Window* wnd = ...;
+// will set Alpha to 100% but is slower due to the casts
+wnd->setProperty<int>("Alpha", 1);
+// retrieves Alpha as string, then proceeds to convert that to int
+// beware of rounding errors!
+wnd->getProperty<int>("Alpha");
+\endcode
+
+We will always offer string fallback mode because it is necessary for serialisation
+and scripting. Scripting languages are often duck typed and cannot use C++
+templated code.
+
+The CEGUI::Property instances hold pointers to getter and setter of
+a given property. They are designed in such a way that multiple PropertySets
+(multiple widgets) can share the Property instances to save memory. This means
+that PropertySets don't own the Properties themselves. The Property instances
+are usually static members of the PropertySet classes or static local
+variables in the scope of the constructor or a method called from there.
+
+It's unusual but multiple instances of the same class can have different
+Properties added to them.
+
+It is recommended to use the \a CEGUI_DEFINE_PROPERTY macro instead of using
+PropertySet::addProperty directly. This takes care of property initialisation
+as well as it's addition to the PropertySet instance.
 */
 class CEGUIEXPORT PropertySet : public PropertyReceiver
 {
 public:
-	/*!
+    /*!
 	\brief
 		Constructs a new PropertySet object
 	*/
-	PropertySet(void) {}
+    PropertySet(void) {}
 
 
-	/*!
+    /*!
 	\brief
 		Destructor for PropertySet objects.
 	*/
-	virtual ~PropertySet(void) {}
+    virtual ~PropertySet(void) {}
 
 
-	/*!
+    /*!
 	\brief
 		Adds a new Property to the PropertySet
 
 	\param property
 		Pointer to the Property object to be added to the PropertySet.
 
-	\return
-		Nothing.
-
 	\exception NullObjectException		Thrown if \a property is NULL.
 	\exception AlreadyExistsException	Thrown if a Property with the same name as \a property already exists in the PropertySet
+
+    \note
+        You most likely don't want to use this method unless you are implementing
+        a custom Property class. Please see CEGUI_DEFINE_PROPERTY instead.
 	*/
-	void	addProperty(Property* property);
+    void addProperty(Property* property);
 
 
-	/*!
+    /*!
 	\brief
 		Removes a Property from the PropertySet.
 
 	\param name
 		String containing the name of the Property to be removed.  If Property \a name is not in the set, nothing happens.
-
-	\return
-		Nothing.
 	*/
-	void	removeProperty(const String& name);
+    void removeProperty(const String& name);
 
 
-	/*!
+    /*!
     \brief
         Retrieves a property instance (that was previously added)
 
@@ -109,20 +158,17 @@ public:
     \return
         Pointer to the property instance
     */
-	Property* getPropertyInstance(const String& name) const;
+    Property* getPropertyInstance(const String& name) const;
 
 
-	/*!
+    /*!
 	\brief
 		Removes all Property objects from the PropertySet.
-
-	\return
-		Nothing.
-	*/
-	void	clearProperties(void);
+    */
+    void clearProperties(void);
 
 
-	/*!
+    /*!
 	\brief
 		Checks to see if a Property with the given name is in the PropertySet
 
@@ -132,10 +178,10 @@ public:
 	\return
 		true if a Property named \a name is in the PropertySet.  false if no Property named \a name is in the PropertySet.
 	*/
-	bool	isPropertyPresent(const String& name) const;
+    bool isPropertyPresent(const String& name) const;
 
 
-	/*!
+    /*!
 	\brief
 		Return the help text for the specified Property.
 
@@ -147,10 +193,10 @@ public:
 
 	\exception UnknownObjectException	Thrown if no Property named \a name is in the PropertySet.
 	*/
-	const String&	getPropertyHelp(const String& name) const;
+    const String& getPropertyHelp(const String& name) const;
 
 
-	/*!
+    /*!
 	\brief
 		Gets the current value of the specified Property.
 
@@ -162,7 +208,7 @@ public:
 
 	\exception UnknownObjectException	Thrown if no Property named \a name is in the PropertySet.
 	*/
-	String	getProperty(const String& name) const;
+    String getProperty(const String& name) const;
 
     /*!
     \copydoc PropertySet::getProperty
@@ -182,20 +228,20 @@ public:
 
         Property* baseProperty = pos->second;
         TypedProperty<T>* typedProperty = dynamic_cast<TypedProperty<T>* >(baseProperty);
-        
+
         if (typedProperty)
         {
             // yay, we can get native!
             return typedProperty->getNative(this);
         }
         else
-        {   
+        {
             // fall back to string get
             return PropertyHelper<T>::fromString(baseProperty->get(this));
         }
     }
 
-	/*!
+    /*!
 	\brief
 		Sets the current value of a Property.
 
@@ -205,13 +251,10 @@ public:
 	\param value
 		String containing a textual representation of the new value for the Property
 
-	\return
-		Nothing
-
 	\exception UnknownObjectException	Thrown if no Property named \a name is in the PropertySet.
 	\exception InvalidRequestException	Thrown when the Property was unable to interpret the content of \a value.
 	*/
-	void	setProperty(const String& name, const String& value);
+    void setProperty(const String& name, const String& value);
 
     /*!
     \copydoc PropertySet::setProperty
@@ -231,7 +274,7 @@ public:
 
         Property* baseProperty = pos->second;
         TypedProperty<T>* typedProperty = dynamic_cast<TypedProperty<T>* >(baseProperty);
-        
+
         if (typedProperty)
         {
             // yay, we can set native!
@@ -244,7 +287,7 @@ public:
         }
     }
 
-	/*!
+    /*!
 	\brief
 		Returns whether a Property is at it's default value.
 
@@ -255,10 +298,10 @@ public:
 		- true if the property has it's default value.
 		- false if the property has been modified from it's default value.
 	*/
-	bool	isPropertyDefault(const String& name) const;
+    bool isPropertyDefault(const String& name) const;
 
 
-	/*!
+    /*!
 	\brief
 		Returns the default value of a Property as a String.
 
@@ -268,16 +311,16 @@ public:
 	\return
 		String object containing a textual representation of the default value for this property.
 	*/
-	String	getPropertyDefault(const String& name) const;
+    String getPropertyDefault(const String& name) const;
 
 private:
-	typedef std::map<String, Property*, StringFastLessCompare
+    typedef std::map<String, Property*, StringFastLessCompare
         CEGUI_MAP_ALLOC(String, Property*)> PropertyRegistry;
-	PropertyRegistry	d_properties;
+    PropertyRegistry	d_properties;
 
 
 public:
-	/*************************************************************************
+    /*************************************************************************
 		Iterator stuff
 	*************************************************************************/
     typedef	ConstMapIterator<PropertyRegistry> PropertyIterator;
@@ -291,7 +334,11 @@ public:
 };
 
 /*!
-Example of usage inside addStandardProperties or similar method.
+\brief
+    The prefered way to define properties inside class that inherit CEGUI::Window
+
+Example of usage inside addStandardProperties or a similar method:
+\code{.cpp}
 {
     const String propertyOrigin("MyAwesomeClass"); // this is automatically used by the macro
     // you can also reference WidgetTypeName or any other string if applicable
@@ -300,7 +347,7 @@ Example of usage inside addStandardProperties or similar method.
         "Property to get/set the alpha value of the Window. Value is floating point number.",
         &Window::setAlpha, &Window::getAlpha, 1.0f)
 };
-
+\endcode
 */
 #define CEGUI_DEFINE_PROPERTY(class_type, property_native_type, name, help, setter, getter, default_value)\
 {\
@@ -311,9 +358,16 @@ Example of usage inside addStandardProperties or similar method.
 }
 
 /*!
-Same as CEGUI_DEFINE_PROPERTY but writeXML is set to false
+\brief
+    Similar to CEGUI_DEFINE_PROPERTY but writeXML is set to false
 
-Example of usage inside addStandardProperties or similar method.
+\note
+    Properties that have no setter (null pointer is passed as setter) are automatically
+    banned from XML, there is no need to use this macro for them. Doing so will cause
+    CEGUI to output a warning.
+
+Example of usage inside addStandardProperties or similar method:
+\code{.cpp}
 {
     const String propertyOrigin("MyAwesomeClass"); // this is automatically used by the macro
     // you can also reference WidgetTypeName or any other string if applicable
@@ -322,7 +376,7 @@ Example of usage inside addStandardProperties or similar method.
         "Property to get/set the alpha value of the Window. Value is floating point number.",
         &Window::setAlpha, &Window::getAlpha, 1.0f)
 };
-
+\endcode
 */
 #define CEGUI_DEFINE_PROPERTY_NO_XML(class_type, property_native_type, name, help, setter, getter, default_value)\
 {\
