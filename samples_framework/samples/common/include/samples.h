6d4e5ebe85dbc61ba45cd16c181c5f71022bccf0
Luca Ebach <lucaebach@gmail.com>
2014-08-23 23:46:30 +0200

    MOD: (SAMPLES) moved samples folder into samples_framework

---
+++ samples_framework/samples/common/include/samples.h
@@ -0,0 +1,26 @@
+#ifndef _SamplesListHeader_h_
+#define _SamplesListHeader_h_
+
+#include "Sample.h"
+
+#include "../../samples/CommonDialogs/CommonDialogs.h"
+#include "../../samples/CustomShapesDrawing/CustomShapesDrawing.h"
+#include "../../samples/Demo6/Demo6.h"
+#include "../../samples/DragDrop/DragDrop.h"
+#include "../../samples/EditboxValidation/EditboxValidation.h"
+#include "../../samples/Fonts/Fonts.h"
+#include "../../samples/FormNavigation/FormNavigation.h"
+#include "../../samples/GameMenu/GameMenu.h"
+#include "../../samples/HelloWorld/HelloWorld.h"
+#include "../../samples/Inventory/Inventory.h"
+#include "../../samples/MenuNavigation/MenuNavigation.h"
+#include "../../samples/Minesweeper/Minesweeper.h"
+#include "../../samples/RenderEffects/RenderEffects.h"
+#include "../../samples/RestaurantGame/RestaurantGame.h"
+#include "../../samples/ScrollablePane/ScrollablePane.h"
+#include "../../samples/SVG/SVG.h"
+#include "../../samples/TabControl/TabControl.h"
+#include "../../samples/Text/Text.h"
+#include "../../samples/Tree/Tree.h"
+#include "../../samples/Widgets/Widgets.h"
+#endif
