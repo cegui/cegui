a6b2b565043e3447b59638b25150e20c1993c6b1
Lukas Meindl <bitbucket@lukasmeindl.at>
2014-08-30 09:50:46 +0200

    MOD: Use initialValue of PropertyDefinitions and PropertyLinkDefinitions This had to be done so the unchangable Property value "d_default" is not used in case the PropertyDefinition or PropertyLinkDefinition has changed

--- cegui/include/CEGUI/falagard/PropertyLinkDefinition.h
+++ cegui/include/CEGUI/falagard/PropertyLinkDefinition.h
@@ -106,7 +106,7 @@ public:
     //------------------------------------------------------------------------//
     void initialisePropertyReceiver(PropertyReceiver* receiver) const
     {
-        updateLinkTargets(receiver, Helper::fromString(this->d_default));
+        updateLinkTargets(receiver, Helper::fromString(PropertyDefinitionBase::d_initialValue));
     }
 
     //------------------------------------------------------------------------//
@@ -128,7 +128,7 @@ protected:
 
         // if no target, or target (currently) invalid, return the default value
         if (d_targets.empty() || !target_wnd)
-            return Helper::fromString(TypedProperty<T>::d_default);
+            return Helper::fromString(PropertyDefinitionBase::d_initialValue);
 
         // otherwise return the value of the property for first target, since
         // this is considered the 'master' target for get operations.
