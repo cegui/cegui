55e7fb7dd53dc097d630ed097f33af5a5345ad67
David Reepmeyer <djreep81@gmail.com>
2014-09-02 22:51:58 -0500

    opengles renderer changes to compile

--- cegui/include/CEGUI/RendererModules/OpenGLES/GeometryBuffer.h
+++ cegui/include/CEGUI/RendererModules/OpenGLES/GeometryBuffer.h
@@ -70,6 +70,8 @@ public:
     uint getBatchCount() const;
     void setRenderEffect(RenderEffect* effect);
     RenderEffect* getRenderEffect();
+    void setClippingActive(const bool active);
+    bool isClippingActive() const;
 
     //! return the GL modelview matrix used for this buffer.
 	const float* getMatrix() const;
@@ -103,6 +105,8 @@ protected:
     VertexList d_vertices;
     //! rectangular clip region
     Rectf d_clipRect;
+    //! whether clipping will be active for the current batch
+    bool d_clippingActive;
     //! translation vector
     Vector3f d_translation;
     //! rotation quaternion
