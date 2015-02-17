64540b1b46bc7b4b89d4d8bf5846f5567ae1c29a
David Reepmeyer <djreep81@gmail.com>
2015-02-17 02:28:14 -0600

    GLM include fix for different GLM versions

--- cegui/src/RenderTarget.cpp
+++ cegui/src/RenderTarget.cpp
@@ -30,6 +30,9 @@
 #include "CEGUI/RenderQueue.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#if (GLM_VERSION_MAJOR == 0 && GLM_VERSION_MINOR <= 9 && GLM_VERSION_PATCH <= 3)                                      
+#include <glm/gtx/constants.hpp>                                                                                      
+#endif
 #include <glm/gtc/type_ptr.hpp>
 
 namespace CEGUI
