c9cdc1b5e64141a7db1f1ad1e67d162e9188fbeb
Lukas Meindl <bitbucket@lukasmeindl.at>
2015-04-21 20:49:46 +0200

    FIX: Tooltips causes issues due to not being pass-through. Also fixed XML print. Regarding XML-printing: The Tooltip texts were accessed wrongly using a single function wheras in reality two functions would be needed, so that the real value can be accessed to and not just the realOrInherited value.

--- cegui/src/widgets/Tooltip.cpp
+++ cegui/src/widgets/Tooltip.cpp
@@ -66,6 +66,7 @@ namespace CEGUI
         setClippedByParent(false);
         setDestroyedByParent(false);
         setAlwaysOnTop(true);
+        setMousePassThroughEnabled(true);
         
         // we need updates even when not visible
         setUpdateMode(WUM_ALWAYS);
@@ -146,7 +147,7 @@ namespace CEGUI
             }
 
             // set text to that of the tooltip text of the target
-            setText(wnd->getTooltipText());
+            setText(wnd->getTooltipTextIncludingInheritance());
 
             // set size and position of the tooltip window.
             sizeSelf();
@@ -237,7 +238,7 @@ namespace CEGUI
     void Tooltip::doActiveState(float elapsed)
     {
         // if no target, switch immediately to inactive state.
-        if (!d_target || d_target->getTooltipText().empty())
+        if (!d_target || d_target->getTooltipTextIncludingInheritance().empty())
         {
             // hide immediately since the text is empty
             hide();
@@ -254,7 +255,7 @@ namespace CEGUI
 
     void Tooltip::doInactiveState(float elapsed)
     {
-        if (d_target && !d_target->getTooltipText().empty() && ((d_elapsed += elapsed) >= d_hoverTime))
+        if (d_target && !d_target->getTooltipTextIncludingInheritance().empty() && ((d_elapsed += elapsed) >= d_hoverTime))
         {
             switchToActiveState();
         }
