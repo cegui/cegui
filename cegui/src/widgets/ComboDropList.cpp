f3ee138775b64dd37a82a93f76666811a5e5c15a
Lukas Meindl <bitbucket@lukasmeindl.at>
2015-04-21 20:52:29 +0200

    FIX: Fixed redundant XML-printing of forwarded AutoWindow properties

--- cegui/src/widgets/ComboDropList.cpp
+++ cegui/src/widgets/ComboDropList.cpp
@@ -83,8 +83,8 @@ void ComboDropList::initialiseComponents(void)
 	getHorzScrollbar()->setRestoreOldCapture(true);
 
     // ban these properties from being written
-    getVertScrollbar()->banPropertyFromXMLRecursive("RestoreOldCapture");
-    getHorzScrollbar()->banPropertyFromXMLRecursive("RestoreOldCapture");
+    getVertScrollbar()->banPropertyFromXMLRecursive(Window::RestoreOldCapturePropertyName);
+    getHorzScrollbar()->banPropertyFromXMLRecursive(Window::RestoreOldCapturePropertyName);
 }
 
 //----------------------------------------------------------------------------//
