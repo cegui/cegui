49b3fd9d6f0d7555198379514b155bcd61daef67
Vladimir Orlov <niello11@gmail.com>
2016-09-03 13:56:38 +0200

    TinyXML2-based parser

---
+++ cegui/src/XMLParserModules/TinyXML2/XMLParser.cpp
@@ -0,0 +1,119 @@
+/***********************************************************************
+    filename:   XMLParser.cpp
+    created:    Fri Sep 02 2016
+    author:     Vladimir "Niello" Orlov
+*************************************************************************/
+/***************************************************************************
+ *   Copyright (C) 2004 - 2016 Paul D Turner & The CEGUI Development Team
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining
+ *   a copy of this software and associated documentation files (the
+ *   "Software"), to deal in the Software without restriction, including
+ *   without limitation the rights to use, copy, modify, merge, publish,
+ *   distribute, sublicense, and/or sell copies of the Software, and to
+ *   permit persons to whom the Software is furnished to do so, subject to
+ *   the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be
+ *   included in all copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ *   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ *   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ *   IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ *   OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ *   OTHER DEALINGS IN THE SOFTWARE.
+ ***************************************************************************/
+#include "CEGUI/XMLParserModules/TinyXML2/XMLParser.h"
+#include "CEGUI/ResourceProvider.h"
+#include "CEGUI/XMLHandler.h"
+#include "CEGUI/XMLAttributes.h"
+#include <tinyxml2.h>
+
+// Start of CEGUI namespace section
+namespace CEGUI
+{
+    TinyXML2Parser::TinyXML2Parser(void)
+    {
+        // set ID string
+        d_identifierString = "CEGUI::TinyXML2Parser - TinyXML2 CEGUI XML parser implementation by Vladimir 'Niello' Orlov";
+    }
+
+    TinyXML2Parser::~TinyXML2Parser(void)
+    {
+    }
+
+    void TinyXML2Parser::parseXML(XMLHandler& handler, const RawDataContainer& source, const String& schemaName)
+    {
+        // Parse the document
+        tinyxml2::XMLDocument doc;
+        tinyxml2::XMLError Result = doc.Parse((const char*)source.getDataPtr(), source.getSize());
+        if (Result == tinyxml2::XML_ERROR_EMPTY_DOCUMENT) return;
+
+        // Check if any actual error occured
+        if (Result != tinyxml2::XML_SUCCESS)
+        {
+            CEGUI_THROW(FileIOException("TinyXML2Parser: an error occurred while "
+                "parsing the XML document '...' - check it for potential errors!"));
+        }
+
+        // Parse root element recursively
+        const tinyxml2::XMLElement* currElement = doc.RootElement();
+        if (currElement)
+        {
+            CEGUI_TRY
+            {
+                processElement(handler, currElement);
+            }
+            CEGUI_CATCH(...)
+            {
+                CEGUI_RETHROW;
+            }
+        }
+    }
+
+    void TinyXML2Parser::processElement(XMLHandler& handler, const tinyxml2::XMLElement* element)
+    {
+        // Build attributes block for the element
+        XMLAttributes attrs;
+
+        const tinyxml2::XMLAttribute *currAttr = element->FirstAttribute();
+        while (currAttr)
+        {
+            attrs.add((utf8*)currAttr->Name(), (utf8*)currAttr->Value());
+            currAttr = currAttr->Next();
+        }
+
+        handler.elementStart((utf8*)element->Value(), attrs);
+
+        const tinyxml2::XMLNode* pChildNode = element->FirstChild();
+        while (pChildNode)
+        {
+            if (pChildNode->ToText())
+            {
+                const char* pValue = pChildNode->ToText()->Value();
+                if (pValue && *pValue)
+                    handler.text((utf8*)pValue);
+            }
+            else if (pChildNode->ToElement())
+                processElement(handler, pChildNode->ToElement());
+            
+            // Silently ignore unhandled node type
+
+            pChildNode = pChildNode->NextSibling();
+        }
+
+        handler.elementEnd((utf8*)element->Value());
+    }
+    
+    bool TinyXML2Parser::initialiseImpl(void)
+    {
+        return true;
+    }
+
+    void TinyXML2Parser::cleanupImpl(void)
+    {
+    }
+
+} // End of  CEGUI namespace section
---
+++ cmake/FindTinyXML2.cmake
@@ -0,0 +1,43 @@
+################################################################################
+# Custom cmake module for CEGUI to find tinyxml2
+################################################################################
+include(FindPackageHandleStandardArgs)
+include(CheckCXXSourceCompiles)
+
+find_path(TINYXML2_H_PATH NAMES tinyxml2.h)
+find_library(TINYXML2_LIB NAMES tinyxml2 libtinyxml2 PATH_SUFFIXES dynamic)
+find_library(TINYXML2_LIB_DBG NAMES tinyxml2_d libtinyxml2_d PATH_SUFFIXES dynamic)
+mark_as_advanced(TINYXML2_H_PATH TINYXML2_LIB TINYXML2_LIB_DBG)
+
+if (WIN32 OR APPLE)
+    find_library(TINYXML2_LIB_STATIC NAMES tinyxml2 libtinyxml2 PATH_SUFFIXES static)
+    find_library(TINYXML2_LIB_STATIC_DBG NAMES tinyxml2_d libtinyxml2_d PATH_SUFFIXES static)
+    mark_as_advanced(TINYXML2_LIB_STATIC TINYXML2_LIB_STATIC_DBG)
+endif()
+
+cegui_find_package_handle_standard_args(TINYXML2 TINYXML2_LIB TINYXML2_H_PATH)
+
+if (TINYXML2_FOUND)
+    # what API version do we have here?
+    set(CMAKE_REQUIRED_INCLUDES ${TINYXML2_H_PATH})
+    set(CMAKE_REQUIRED_LIBRARIES ${TINYXML2_LIB})
+
+    set (TINYXML2_INCLUDE_DIR ${TINYXML2_H_PATH})
+    set (TINYXML2_LIBRARIES ${TINYXML2_LIB})
+    if (TINYXML2_LIB_DBG)
+        set (TINYXML2_LIBRARIES_DBG ${TINYXML2_LIB_DBG})
+    endif()
+    if (TINYXML2_LIB_STATIC)
+        set (TINYXML2_LIBRARIES_STATIC ${TINYXML2_LIB_STATIC})
+    endif()
+    if (TINYXML2_LIB_STATIC_DBG)
+        set (TINYXML2_LIBRARIES_STATIC_DBG ${TINYXML2_LIB_STATIC_DBG})
+    endif()
+else()
+    set (TINYXML2_INCLUDE_DIR)
+    set (TINYXML2_LIBRARIES)
+    set (TINYXML2_LIBRARIES_DBG)
+    set (TINYXML2_LIBRARIES_STATIC)
+    set (TINYXML2_LIBRARIES_STATIC_DBG)
+endif()
+
