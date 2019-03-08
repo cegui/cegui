a05ed827b3c5829224180747a432f9e44436b1b4
??????? ??????? <zkolosok@yandex.ru>
2019-03-08 13:58:21 +0000

    MemoryFile.h edited online with Bitbucket

--- cegui/include/CEGUI/RendererModules/Irrlicht/MemoryFile.h
+++ cegui/include/CEGUI/RendererModules/Irrlicht/MemoryFile.h
@@ -52,7 +52,7 @@ public:
     virtual ~IrrlichtMemoryFile() {};
 
     // implement required interface from IReadFile
-    size_t read(void* buffer, size_t sizeToRead);
+    std::size_t read(void* buffer, std::size_t sizeToRead);
     long getSize() const;
     long getPos() const;
     bool seek(long finalPos, bool relativeMovement = false);
