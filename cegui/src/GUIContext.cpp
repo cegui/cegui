70997d9877806a70a66f1f871514b2264931fae7
Florian <fluxey@gmail.com>
2019-12-13 10:14:52 +0000

    GUIContext.cpp: add missing EventSelectWord event

--- cegui/src/GUIContext.cpp
+++ cegui/src/GUIContext.cpp
@@ -793,6 +793,30 @@ bool GUIContext::handleCursorPressHoldEvent(const SemanticInputEvent& event)
 }
 
 //----------------------------------------------------------------------------//
+bool GUIContext::handleSelectWord(const SemanticInputEvent& event)
+{
+    CursorInputEventArgs ciea(nullptr);
+    ciea.position = d_cursor.getPosition();
+    ciea.moveDelta = glm::vec2(0, 0);
+    ciea.source = event.d_payload.source;
+    ciea.scroll = 0;
+    ciea.window = getTargetWindow(ciea.position, false);
+    // make cursor position sane for this target window
+    if (ciea.window)
+        ciea.position = ciea.window->getUnprojectedPosition(ciea.position);
+
+    // if there is no target window, input can not be handled.
+    if (!ciea.window)
+        return false;
+
+    if (d_windowNavigator != nullptr)
+        d_windowNavigator->setCurrentFocusedWindow(ciea.window);
+
+    ciea.window->onSelectWord(ciea);
+    return ciea.handled != 0;
+}
+
+//----------------------------------------------------------------------------//
 bool GUIContext::handleScrollEvent(const SemanticInputEvent& event)
 {
     CursorInputEventArgs ciea(nullptr);
