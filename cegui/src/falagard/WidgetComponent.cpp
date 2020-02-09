cegui/cegui

commit 980c4f700e8996a5b126379772d10d297313b18c

Updates after review  comments

cegui/src/falagard/WidgetComponent.cpp
@@ -173,15 +173,28 @@ namespace CEGUI
     {
         PropertyInitialiserList::iterator f = d_propertyInitialisers.begin();
         PropertyInitialiserList::iterator const l = d_propertyInitialisers.end();
+        // look for any removal candidate
         for(; f != l; ++f)
+        {
             if(f->getTargetPropertyName() == name)
+            {
                 break;
-        if(f == l) return;
+            }
+        }
+        if(f == l)
+        {
+            // nothing to remove, so done
+            return;
+        }
+        // start moving over any remaining items to keep
         for(PropertyInitialiserList::iterator i = f; ++i != l;)
-            if(f->getTargetPropertyName() != name) {
-                *f = *i;
+        {
+            if(f->getTargetPropertyName() != name)
+            {
+                std::iter_swap(f, i);
                 ++f;
             }
+        }
         d_propertyInitialisers.erase(f, l);
     }
 

cegui/src/ImageCodecModules/STB/stb_image.cpp
@@ -3298,7 +3298,8 @@ static stbi_uc *tga_load(stbi *s, int *x, int *y, int *comp, int req_comp)
       skip(s, tga_palette_start );
       //   load the palette
       tga_palette = (unsigned char*)malloc( tga_palette_len * tga_palette_bits / 8 );
-      if (!tga_palette) {
+      if (!tga_palette)
+      {
          free(tga_data);
          return epuc("outofmem", "Out of memory");
       }

commit 5c335ab33bc54ddef5484afcd8b6c5c83b9a0a13

Fix for bug introduced with 980c4f7

cegui/src/falagard/WidgetComponent.cpp
@@ -189,7 +189,7 @@ namespace CEGUI
         // start moving over any remaining items to keep
         for(PropertyInitialiserList::iterator i = f; ++i != l;)
         {
-            if(f->getTargetPropertyName() != name)
+            if(i->getTargetPropertyName() != name)
             {
                 std::iter_swap(f, i);
                 ++f;