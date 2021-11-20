8dc542f0b3f6c806f54bda024933c146491ffed0
Vladimir Orlov <niello@bk.ru>
2021-11-20 13:47:38 +0200

    Deprecated macros removed

--- cegui/include/CEGUI/WindowFactory.h
+++ cegui/include/CEGUI/WindowFactory.h
@@ -33,60 +33,6 @@
 #include "CEGUI/String.h"
 #include "CEGUI/Window.h"
 
-/*!
-\brief
-    Declares a window factory class.
-
-\param T
-    The window class name.
-
-\note
-    The class that will be generated is is named &lt;classname&gt;Factory.
-    A statement like this:
-        CEGUI_DECLARE_WINDOW_FACTORY(MyWidget);
-    Would generate a factory class named MyWidgetFactory.
-
-    The factory is automatically instantiated and for the example it would
-    be available as:
-        WindowFactory* wf = &(getMyWidgetFactory());
-    or
-        WindowFactory* wf = &CEGUI_WINDOW_FACTORY(MyWidget);
-*/
-#define CEGUI_DECLARE_WINDOW_FACTORY( T )\
-    class T ## Factory : public WindowFactory\
-    {\
-    public:\
-        T ## Factory() : WindowFactory( T::WidgetTypeName ) {}\
-        Window* createWindow(const String& name)\
-        {\
-            return new T(d_type, name);\
-        }\
-        void destroyWindow(Window* window)\
-        {\
-            delete window;\
-        }\
-    };\
-    T ## Factory& get ## T ## Factory();
-
-/*!
-\brief
-    Generates code for the constructor for the instance of the window factory
-    generated from the class name \a T
-*/
-#define CEGUI_DEFINE_WINDOW_FACTORY( T )\
-    T ## Factory& get ## T ## Factory()\
-    {\
-        static T ## Factory s_factory;\
-        return s_factory;\
-    }
-
-/*!
-\brief
-    Helper macro that return the real factory class name from a given class
-    name \a T
-*/
-#define CEGUI_WINDOW_FACTORY( T ) (get ## T ## Factory())
-
 // Start of CEGUI namespace section
 namespace CEGUI
 {
@@ -101,13 +47,7 @@ namespace CEGUI
     WindowFactoryManager so that the system knows how to create and destroy
     those types of Window base object.
 
-\note
-    The use if of the CEGUI_DECLARE_WINDOW_FACTORY, CEGUI_DEFINE_WINDOW_FACTORY
-    and CEGUI_WINDOW_FACTORY macros is deprecated in favour of the
-    template class TplWindowFactory and templatised
-    WindowFactoryManager::addFactory function, whereby you no longer need to
-    directly create any supporting structure for your new window type, and can
-    simply do:
+    To register a simple default factory for the new widget type, use:
     \code
     CEGUI::WindowFactoryManager::addFactory<TplWindowFactory<MyWidget> >();
     \endcode
@@ -148,12 +88,10 @@ public:
         String object that contains the unique Window object type produced by
         this WindowFactory
     */
-    const String& getTypeName() const
-    { return d_type; }
+    const String& getTypeName() const { return d_type; }
 
     //! Destructor.
-    virtual ~WindowFactory()
-    {}
+    virtual ~WindowFactory() = default;
 
 protected:
     //! Constructor
