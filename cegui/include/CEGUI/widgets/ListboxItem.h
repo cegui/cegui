f265958deeb3d80beae79c7addc1e62464cf4db3
Vladimir Orlov <niello@bk.ru>
2022-02-02 18:30:20 +0200

    Listbox text item rendering fix

--- cegui/include/CEGUI/widgets/ListboxItem.h
+++ cegui/include/CEGUI/widgets/ListboxItem.h
@@ -343,7 +343,7 @@ public:
     \param clipper
         Rect object describing the clipping rectangle for the draw operation.
     */
-    virtual void createRenderGeometry(std::vector<GeometryBuffer*> out,
+    virtual void createRenderGeometry(std::vector<GeometryBuffer*>& out,
         const Rectf& targetRect,
         float alpha, const Rectf* clipper) const = 0;
 
