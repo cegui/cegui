3a14c80f3b424e07e496d1344f76e2ced56824ce
Christoph Brill <opensource@christophbrill.de>
2022-06-13 22:24:00 +0200

    Add missing include to sstream

--- cegui/src/RendererModules/Ogre/Texture.cpp
+++ cegui/src/RendererModules/Ogre/Texture.cpp
@@ -24,6 +24,7 @@
  *   ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
  *   OTHER DEALINGS IN THE SOFTWARE.
  ***************************************************************************/
+#include <sstream>
 #include "CEGUI/RendererModules/Ogre/Texture.h"
 #include "CEGUI/Exceptions.h"
 #include "CEGUI/System.h"
